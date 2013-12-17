5c77e4be78fc106ee17b397d9151ceed0eb877a3
darkkiero <paul.vankeuren@gmail.com>
2013-12-17 12:50:30 -0600

    fixed some paths in publish_wercker

--- tools/Publish_wercker.dart
+++ tools/Publish_wercker.dart
@@ -25,56 +25,56 @@ import 'dart:io';
 main() {
   
 
-        if (new Directory('../out').existsSync() == false)
-          new Directory('../out').createSync(recursive: true);
-   new Directory('../out').deleteSync(recursive: true);
-   new Directory('../out/web').createSync(recursive: true);
+        if (new Directory('./out').existsSync() == false)
+          new Directory('./out').createSync(recursive: true);
+   new Directory('./out').deleteSync(recursive: true);
+   new Directory('./out/web').createSync(recursive: true);
   
   // Places our js in the output folder
-  new File('../out/web/game.js').writeAsStringSync(
-      new File('../web/game.js').readAsStringSync());
+  new File('./out/web/game.js').writeAsStringSync(
+      new File('./web/game.js').readAsStringSync());
   
   // Places our html in the output folder
-  new File('../out/web/game.html').writeAsStringSync(
+  new File('./out/web/game.html').writeAsStringSync(
       minifyHtml(
-          new File('../web/game.html').readAsLinesSync()));
+          new File('./web/game.html').readAsLinesSync()));
   
       
   // Create css and font folders
-  new Directory('../out/web/css/font').createSync(recursive: true);
+  new Directory('./out/web/css/font').createSync(recursive: true);
       
   // Places our css in the output folder
-  new File('../out/web/css/base.css').writeAsStringSync(
+  new File('./out/web/css/base.css').writeAsStringSync(
       minifyCss(
-        new File('../web/css/base.css').readAsLinesSync()
+        new File('./web/css/base.css').readAsLinesSync()
           ));
    
   // Copies FontAwesome to the output folder
-  new File('../out/web/css/font-awesome.min.css').writeAsBytesSync(
-        new File('../web/css/font-awesome.min.css').readAsBytesSync());
-  new File('../out/web/css/font/fontawesome-webfont.eot').writeAsBytesSync(
-        new File('../web/css/font/fontawesome-webfont.eot').readAsBytesSync());
-  new File('../out/web/css/font/fontawesome-webfont.svg').writeAsBytesSync(
-        new File('../web/css/font/fontawesome-webfont.svg').readAsBytesSync());
-  new File('../out/web/css/font/fontawesome-webfont.ttf').writeAsBytesSync(
-        new File('../web/css/font/fontawesome-webfont.ttf').readAsBytesSync());
-  new File('../out/web/css/font/fontawesome-webfont.woff').writeAsBytesSync(
-        new File('../web/css/font/fontawesome-webfont.woff').readAsBytesSync());
-  new File('../out/web/css/font/FontAwesome.otf').writeAsBytesSync(
-        new File('../web/css/font/FontAwesome.otf').readAsBytesSync());
+  new File('./out/web/css/font-awesome.min.css').writeAsBytesSync(
+        new File('./web/css/font-awesome.min.css').readAsBytesSync());
+  new File('./out/web/css/font/fontawesome-webfont.eot').writeAsBytesSync(
+        new File('./web/css/font/fontawesome-webfont.eot').readAsBytesSync());
+  new File('./out/web/css/font/fontawesome-webfont.svg').writeAsBytesSync(
+        new File('./web/css/font/fontawesome-webfont.svg').readAsBytesSync());
+  new File('./out/web/css/font/fontawesome-webfont.ttf').writeAsBytesSync(
+        new File('./web/css/font/fontawesome-webfont.ttf').readAsBytesSync());
+  new File('./out/web/css/font/fontawesome-webfont.woff').writeAsBytesSync(
+        new File('./web/css/font/fontawesome-webfont.woff').readAsBytesSync());
+  new File('./out/web/css/font/FontAwesome.otf').writeAsBytesSync(
+        new File('./web/css/font/FontAwesome.otf').readAsBytesSync());
    
 // Deletes the unneeded files made when we used dart2js
-  print('Cleaning Workspace...');
-  new File('../web/game.js').deleteSync();
-  new File('../web/game.js.deps').deleteSync();
-  new File('../web/game.js.map').deleteSync();
-  new File('../web/game.precompiled.js').deleteSync();
-  print('...Done');
+  print('Cleaning Workspace.');
+  new File('./web/game.js').deleteSync();
+  new File('./web/game.js.deps').deleteSync();
+  new File('./web/game.js.map').deleteSync();
+  new File('./web/game.precompiled.js').deleteSync();
+  print('.Done');
 }
 
 // Sets up the outputted html file to use JS instead of Dart, Plus optimizing media paths.
 List <String> modeJS(List<String> fileLines){
-  print('Converting HTML file to use JavaScript...');
+  print('Converting HTML file to use JavaScript.');
   List<String> newHTML = new List();
   for (String line in fileLines)
   {
@@ -89,7 +89,7 @@ List <String> modeJS(List<String> fileLines){
 
 
 String minifyCss(List<String> fileLines){
-  print('Minifying CSS...');  
+  print('Minifying CSS.');  
   // Remove Comments
   fileLines.removeWhere((e) => e.startsWith('/*'));
   
@@ -111,7 +111,7 @@ String minifyCss(List<String> fileLines){
 
 String minifyHtml(List<String> input){
   
-   print('Converting HTML file to use JavaScript...');
+   print('Converting HTML file to use JavaScript.');
    List<String> fileLines = new List();
    for (String line in input)
    {
@@ -124,7 +124,7 @@ String minifyHtml(List<String> input){
 
   
   
-    print('Minifying HTML...');  
+    print('Minifying HTML.');  
   
     // Remove Comments
     fileLines.removeWhere((e) => e.startsWith('<!--'));
