af2bb10e3f16f1caff45654e10ae9a493653a260
Andy C <andyccastille@gmail.com>
2014-03-01 08:44:11 -0600

    Update chat_bubble.dart

--- web/dart/chat_bubble.dart
+++ web/dart/chat_bubble.dart
@@ -9,7 +9,7 @@ class ChatBubble
 	
 	ChatBubble(this.text)
 	{
-		timeToLive = text.length * 0.1 + 3;
+		timeToLive = text.length * 0.1 + 3; //minimum 3s plus 0.1s per character
 		bubble = new DivElement()
 			..classes.add("PlayerChatBubble");
 		textElement = new SpanElement()
