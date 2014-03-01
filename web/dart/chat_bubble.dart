5ce6c3ca2aff6b2ad73fce5f8d68ab5cfef9bbe2
Andy C <andyccastille@gmail.com>
2014-03-01 11:07:27 -0600

    added max 15s

--- web/dart/chat_bubble.dart
+++ web/dart/chat_bubble.dart
@@ -10,6 +10,9 @@ class ChatBubble
   ChatBubble(this.text)
   {
     timeToLive = text.length * 0.1 + 3; //minimum 3s plus 0.1s per character
+    if (timeToLive > 15) {
+      timeToLive = 15;
+    }
     bubble = new DivElement()
       ..classes.add("PlayerChatBubble");
     textElement = new SpanElement()
