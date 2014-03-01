d79439f6eff443d95b4f149f37f7b8ab37db460a
Andy C <andyccastille@gmail.com>
2014-03-01 11:00:35 -0600

    added max 20s

--- web/dart/chat_bubble.dart
+++ web/dart/chat_bubble.dart
@@ -2,19 +2,22 @@ part of coUclient;
 
 class ChatBubble
 {
-	String text;
-	num timeToLive;
-	DivElement bubble;
-	SpanElement textElement;
-	
-	ChatBubble(this.text)
-	{
-		timeToLive = text.length * 0.1 + 3; //minimum 3s plus 0.1s per character
-		bubble = new DivElement()
-			..classes.add("PlayerChatBubble");
-		textElement = new SpanElement()
-			..style.display = "inline-block"
-			..text = text;
-		bubble.append(textElement);
-	}
+  String text;
+  num timeToLive;
+  DivElement bubble;
+  SpanElement textElement;
+
+  ChatBubble(this.text)
+  {
+    timeToLive = text.length * 0.1 + 3; //minimum 3s plus 0.1s per character
+    if (timeToLive >= 21 {
+      timeToLive = 20;
+    }
+    bubble = new DivElement()
+      ..classes.add("PlayerChatBubble");
+    textElement = new SpanElement()
+      ..style.display = "inline-block"
+      ..text = text;
+    bubble.append(textElement);
+  }
 }
