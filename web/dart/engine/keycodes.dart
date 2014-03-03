37aa6dc791443588e9627aa6277aa589d7a49b90
Robert McDermot <robert.mcdermot@gmail.com>
2014-03-03 16:53:10 -0500

    forgot to add keycodes.dart to git

---
+++ web/dart/engine/keycodes.dart
@@ -0,0 +1,107 @@
+part of coUclient;
+
+String fromKeyCode(int keyCode)
+{
+	String keyPressed;
+	if (keyCode == 8) keyPressed = "backspace"; //  backspace
+	if (keyCode == 9) keyPressed = "tab"; //  tab
+	if (keyCode == 13) keyPressed = "enter"; //  enter
+	if (keyCode == 16) keyPressed = "shift"; //  shift
+	if (keyCode == 17) keyPressed = "ctrl"; //  ctrl
+	if (keyCode == 18) keyPressed = "alt"; //  alt
+	if (keyCode == 19) keyPressed = "pause/break"; //  pause/break
+	if (keyCode == 20) keyPressed = "caps lock"; //  caps lock
+	if (keyCode == 27) keyPressed = "escape"; //  escape
+	if (keyCode == 32) keyPressed = "space"; // space;
+	if (keyCode == 33) keyPressed = "page up"; // page up, to avoid displaying alternate character and confusing people
+	if (keyCode == 34) keyPressed = "page down"; // page down
+	if (keyCode == 35) keyPressed = "end"; // end
+	if (keyCode == 36) keyPressed = "home"; // home
+	if (keyCode == 37) keyPressed = "left arrow"; // left arrow
+	if (keyCode == 38) keyPressed = "up arrow"; // up arrow
+	if (keyCode == 39) keyPressed = "right arrow"; // right arrow
+	if (keyCode == 40) keyPressed = "down arrow"; // down arrow
+	if (keyCode == 45) keyPressed = "insert"; // insert
+	if (keyCode == 46) keyPressed = "delete"; // delete
+	if (keyCode == 48) keyPressed = "0"; // 0
+	if (keyCode == 49) keyPressed = "1"; // 1
+	if (keyCode == 50) keyPressed = "2"; // 2
+	if (keyCode == 51) keyPressed = "3"; // 3
+	if (keyCode == 52) keyPressed = "4"; // 4
+	if (keyCode == 53) keyPressed = "5"; // 5
+	if (keyCode == 54) keyPressed = "6"; // 6
+	if (keyCode == 55) keyPressed = "7"; // 7
+	if (keyCode == 56) keyPressed = "8"; // 8
+	if (keyCode == 57) keyPressed = "9"; // 9
+	if (keyCode == 65) keyPressed = "A"; // A
+	if (keyCode == 66) keyPressed = "B"; // B
+	if (keyCode == 67) keyPressed = "C"; // C
+	if (keyCode == 68) keyPressed = "D"; // D
+	if (keyCode == 69) keyPressed = "E"; // E
+	if (keyCode == 70) keyPressed = "F"; // F
+	if (keyCode == 71) keyPressed = "G"; // G
+	if (keyCode == 72) keyPressed = "H"; // H
+	if (keyCode == 73) keyPressed = "I"; // I
+	if (keyCode == 74) keyPressed = "J"; // J
+	if (keyCode == 75) keyPressed = "K"; // K
+	if (keyCode == 76) keyPressed = "L"; // L
+	if (keyCode == 77) keyPressed = "M"; // M
+	if (keyCode == 78) keyPressed = "N"; // N
+	if (keyCode == 79) keyPressed = "O"; // O
+	if (keyCode == 80) keyPressed = "P"; // P
+	if (keyCode == 81) keyPressed = "Q"; // Q
+	if (keyCode == 82) keyPressed = "R"; // R
+	if (keyCode == 83) keyPressed = "S"; // S
+	if (keyCode == 84) keyPressed = "T"; // T
+	if (keyCode == 85) keyPressed = "U"; // U
+	if (keyCode == 86) keyPressed = "V"; // V
+	if (keyCode == 87) keyPressed = "W"; // W
+	if (keyCode == 88) keyPressed = "X"; // X
+	if (keyCode == 89) keyPressed = "Y"; // Y
+	if (keyCode == 90) keyPressed = "Z"; // Z
+	if (keyCode == 91) keyPressed = "left window"; // left window
+	if (keyCode == 92) keyPressed = "right window"; // right window
+	if (keyCode == 93) keyPressed = "select key"; // select key
+	if (keyCode == 96) keyPressed = "numpad 0"; // numpad 0
+	if (keyCode == 97) keyPressed = "numpad 1"; // numpad 1
+	if (keyCode == 98) keyPressed = "numpad 2"; // numpad 2
+	if (keyCode == 99) keyPressed = "numpad 3"; // numpad 3
+	if (keyCode == 100) keyPressed = "numpad 4"; // numpad 4
+	if (keyCode == 101) keyPressed = "numpad 5"; // numpad 5
+	if (keyCode == 102) keyPressed = "numpad 6"; // numpad 6
+	if (keyCode == 103) keyPressed = "numpad 7"; // numpad 7
+	if (keyCode == 104) keyPressed = "numpad 8"; // numpad 8
+	if (keyCode == 105) keyPressed = "numpad 9"; // numpad 9
+	if (keyCode == 106) keyPressed = "multiply"; // multiply
+	if (keyCode == 107) keyPressed = "add"; // add
+	if (keyCode == 109) keyPressed = "subtract"; // subtract
+	if (keyCode == 110) keyPressed = "decimal point"; // decimal point
+	if (keyCode == 111) keyPressed = "divide"; // divide
+	if (keyCode == 112) keyPressed = "F1"; // F1
+	if (keyCode == 113) keyPressed = "F2"; // F2
+	if (keyCode == 114) keyPressed = "F3"; // F3
+	if (keyCode == 115) keyPressed = "F4"; // F4
+	if (keyCode == 116) keyPressed = "F5"; // F5
+	if (keyCode == 117) keyPressed = "F6"; // F6
+	if (keyCode == 118) keyPressed = "F7"; // F7
+	if (keyCode == 119) keyPressed = "F8"; // F8
+	if (keyCode == 120) keyPressed = "F9"; // F9
+	if (keyCode == 121) keyPressed = "F10"; // F10
+	if (keyCode == 122) keyPressed = "F11"; // F11
+	if (keyCode == 123) keyPressed = "F12"; // F12
+	if (keyCode == 144) keyPressed = "num lock"; // num lock
+	if (keyCode == 145) keyPressed = "scroll lock"; // scroll lock
+	if (keyCode == 186) keyPressed = ";"; // semi-colon
+	if (keyCode == 187) keyPressed = "="; // equal-sign
+	if (keyCode == 188) keyPressed = ","; // comma
+	if (keyCode == 189) keyPressed = "-"; // dash
+	if (keyCode == 190) keyPressed = "."; // period
+	if (keyCode == 191) keyPressed = "/"; // forward slash
+	if (keyCode == 192) keyPressed = "`"; // grave accent
+	if (keyCode == 219) keyPressed = "["; // open bracket
+	if (keyCode == 220) keyPressed = "\\"; // back slash
+	if (keyCode == 221) keyPressed = "]"; // close bracket
+	if (keyCode == 222) keyPressed = "'"; // single quote  var lblCharCode = getObject('spnCode');
+     
+     return keyPressed;
+}
