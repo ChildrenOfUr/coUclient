316d4399eaf4d65acdfc2f63f54c2331d5e2bf64
darkkiero <paul.vankeuren@gmail.com>
2014-05-21 12:55:18 -0500

    Attached almost all the elements to a ui class, optimized some overdrawing to remove jank, added a generic button class, fixed a bug where the energy meter wouldn't highlight on mouseover

--- web/main.dart
+++ web/main.dart
@@ -1,35 +1,53 @@
 import 'dart:html';
+import 'dart:async';
 
 import 'package:glitchTime/glitch-time.dart';// The script that spits out time!
 
 main() {
   display
-    ..refreshClock()
-    ..setName('Test Player')
-    ..setMaxEnergy(100)
-    ..setEnergy(100)
-    ..setImg(100);
-  
-  
+    ..init()
+    ..name = 'Paul'
+    ..img = 100
+    ..currants = 100
+    ..energy = 10
+    ..maxenergy = 100
+    ..mood = 10
+    ..maxmood = 100;
+    gameLoop(lastTime);
+}
+// Declare our game_loop
+double lastTime = 0.0;
+gameLoop(num delta)
+{
+  double dt = (delta-lastTime)/1000;
+  display.update();
+  lastTime = delta;
+  window.animationFrame.then(gameLoop);
 }
 
 
-
-
-
-
-
-
 // Strictly for changing appearance
+
 UserInterface display = new UserInterface();
 class UserInterface {
   
   //NumberFormat for having commas in the currants and iMG displays
   //NumberFormat commaFormatter = new NumberFormat("#,###");
   
-  
-  Element youWon = querySelector('youWon');
+  // If you need to change an element somewhere else, put the declaration in this class.
+  // You can then access it with 'ui.yourElement'. This way we keep everything in one spot
+  /////////////////////ELEMENTS//////////////////////////////////////////////
+  // you won! element
+  Element youWon = querySelector('#youWon');
 
+  // Initial play button
+  Element playButton = querySelector('#playButton');
+  
+  // Initial loading screen elements
+  Element loadStatus = querySelector("#loading #loadstatus"); //done
+  Element loadStatus2 = querySelector("#loading #loadstatus2"); //done
+  Element loadingScreen = querySelector('#loading'); //done
+  
   // Name Meter Variables
   Element nameMeter = querySelector('#playerName'); //done
 
@@ -41,7 +59,7 @@ class UserInterface {
   // Currant Meter Variables
   Element currantMeter = querySelector('#currCurrants'); //done
 
-// Img Meter Variables
+  // Img Meter Variables
   Element imgMeter = querySelector('#currImagination');
 
   // Music Meter Variables
@@ -61,52 +79,93 @@ class UserInterface {
   Element currMoodText = querySelector('#moodMeter .fraction .curr'); //done
   Element maxMoodText = querySelector('#moodMeter .fraction .max'); //done
   Element moodPercent = querySelector('#moodMeter .percent .number'); //done
+  /////////////////////ELEMENTS//////////////////////////////////////////////
   
+  
+  // Declare/Set initial variables here
+  /////////////////////VARS//////////////////////////////////////////////////
+  String name;
+  
+  int energy = 0;
+  int maxenergy = 100;  
+  
+  int mood = 100;
+  int maxmood = 100;
+  
+  int currants = 0;  
+  
+  int img = 0;
+  /////////////////////VARS//////////////////////////////////////////////////
+  
+  // start listening for events
   init(){
     window.onBeforeUnload.listen((_) {
       youWon.hidden = false;
     });
-    this.setMaxEnergy(100);
-    this.setEnergy(100);
+    
+    playButton.onClick.listen((_) {
+      loadingScreen.style.opacity = '0';
+      new Timer(new Duration(seconds:1),() => loadingScreen.remove());
+      playButton.remove();
+    });
   }
 
-  setName(String name){
+  // update the userinterface
+  update(){
+    // Update Clock
+    List data = getDate();
+    
+    if (data[4] != currTime.text) {
+      currDay.text = data[3].toString();
+      currTime.text = data[4];
+      currDate.text = data[2].toString() + ' of ' + data[1].toString();
+    }
+
+    // Update img display
+    if (img.toString() != imgMeter.text)
+    imgMeter.text = img.toString();//commaFormatter.format(img);
+    
+    
+    // Update currant display
+    if (currants.toString() != currantMeter.text)
+    currantMeter.text = currants.toString();//commaFormatter.format(currants);
+    
+    // Update mood elements
+    if(maxmood <= 0) {
+      maxmood = 1;   }
+    if (mood.toString() != currMoodText.text || maxmood.toString() != maxMoodText.text) {
+      currMoodText.text=mood.toString();
+      maxMoodText.text=maxmood.toString();
+      moodPercent.text=((100*((mood/maxmood))).toInt()).toString();
+      moodmeterImageLow.style.opacity = ((0.7-(mood/maxmood))).toString();
+      if (mood <= 0)
+        moodmeterImageEmpty.style.opacity = 1.toString();
+      else
+        moodmeterImageEmpty.style.opacity = 0.toString();
+    }
+
+    // Update name display  
     if (name.length >= 17)
         name = name.substring(0, 15) + '...';
+    if (name != nameMeter.text)
       nameMeter.text = name;
+    
+   
+    // Update energy elements
+    if(maxenergy <= 0) {
+      maxenergy = 1;   }
+    if (currEnergyText.text != energy.toString())
+    currEnergyText.text = energy.toString();
+    if (maxEnergyText.text != maxenergy.toString())
+    maxEnergyText.text = maxenergy.toString();
+    String angle = ( (120 - (energy/maxenergy)*120).toInt() ).toString();
+    energymeterImage.style.transform = 'rotate(' +angle+ 'deg)';
+    energymeterImageLow.style.transform = 'rotate(' +angle+ 'deg)';
+    energymeterImageLow.style.opacity = ((1-(energy/maxenergy))).toString();
   }
-  
-  setImg(int img){
-    imgMeter.text = img.toString();//commaFormatter.format(img);
-  }
-  
-  int _maxenergy = 0;
-  int _currenergy = 0;
-  setEnergy(int energy){
-        _currenergy = energy;
-        currEnergyText.text=energy.toString();
-        String angle = ((120 - (energy/_maxenergy)*120).toInt()).toString();
-        energymeterImage.style.transform = 'rotate(' +angle+ 'deg)';
-        energymeterImageLow.style.transform = 'rotate(' +angle+ 'deg)';
-        energymeterImageLow.style.opacity = ((1-(energy/_maxenergy))).toString();    
-  }
-  setMaxEnergy(int energy){
-    _maxenergy = energy;
-    maxEnergyText.text=energy.toString();
-    setEnergy(_currenergy);
-  }
-  
-  setMood(int mood){}
-  setMaxMood(int mood){}
-  
+
   setSong(){}
- 
-  refreshClock() {
-    List data = getDate();
-    currDay.innerHtml = data[3].toString();
-    currTime.innerHtml = data[4].toString();
-    currDate.innerHtml = data[2].toString() + ' of ' + data[1].toString();
-  }
+  
 }
 
 
