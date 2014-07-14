ad2262b14ed1aabc652b6944be47ab75bac4c460
darkkiero <paul.vankeuren@gmail.com>
2014-07-14 14:45:28 -0500

    testing trees

--- assets/packages/bootjack/src/util/util.dart
+++
@@ -1,8 +0,0 @@
-part of bootjack;
-
-Map _copy(Map a, Map b) {
-  Map c = new HashMap.from(a);
-  if (b != null)
-    b.forEach((key, value) => a[key] = value);
-  return c;
-}
--- assets/system/packages/gloss/src/nodes/null.dart
+++
@@ -1,3 +0,0 @@
-part of nodes;
-
-class Null extends Node {}
