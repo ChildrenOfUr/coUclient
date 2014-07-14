d64f1a4b1471e25de662f483ad46d21b135b9bcc
darkkiero <paul.vankeuren@gmail.com>
2014-07-14 13:43:37 -0500

    rearranged some imports and directories

--- web/main.dart
+++ web/main.dart
@@ -2,47 +2,48 @@ library couclient;
 /*
  *  THE CHILDREN OF UR WEBCLIENT
  *  http://www.childrenofur.com
- *  
- * 
- * 
- * 
- * 
  * 
  * 
 */
 
+// DART //
 import 'dart:html';
 import 'dart:async';
 import 'dart:convert';
 
+// LIBRARIES //
 // Used for NumberFormat
 import 'package:intl/intl.dart' show NumberFormat; 
-
 // Slack Webhook API
 import 'package:slack/slack_html.dart' as slack;
-
 // SoundCloud Bootstrap
 import 'package:scproxy/scproxy.dart';
-
 // Asset Loading
 import 'package:libld/libld.dart'; // Nice and simple asset loading.
-
-// Event Bus
+// Event Bus and Pumps // for more infomation see '/doc/pumps.md'
 import 'package:pump/pump.dart';
 
+// SYSTEMS MODULES //
+part 'package:couclient/src/systems/clock.dart';
+part 'package:couclient/src/systems/debug.dart';
+part 'package:couclient/src/systems/events.dart';
 
-// MODULES //
-part 'package:couclient/src/model/clock.dart';
-part 'package:couclient/src/debug.dart';
-part 'package:couclient/src/game/game.dart';
-part 'package:couclient/src/events.dart';
+// NETWORKING MODULES //
+part 'package:couclient/src/network/chat_man.dart';
+
+// UI/UX MODULES
 part 'package:couclient/src/display/ui.dart';
 part 'package:couclient/src/display/audio.dart';
-part 'package:couclient/src/network/chat_man.dart';
+
+// GAME MODULES //
+part 'package:couclient/src/game/game.dart';
+
 
 // API KEYS // for more infomation see '/doc/api.md'
 //part 'API_KEYS.dart';
 
+
+// GAME ENTRY //
 main() {
   app.init();
   new ChatManager();
