1d7db3b70b2566d4de07d7912c4f25c2c253f039
darkkiero <paul.vankeuren@gmail.com>
2014-08-12 12:02:28 -0500

    chat settings toggles now remember state

--- lib/src/display/windows.dart
+++ lib/src/display/windows.dart
@@ -7,6 +7,9 @@ part of couclient;
 class WindowManager {
   Map<String, Modal> modals = {};
 
+  // SETTINGS BOOLS
+  bool _showJoinMessages = false, _playMentionSound = true;
+
   WindowManager() {
 
 
@@ -20,6 +23,10 @@ class WindowManager {
     ui.mapButton.onClick.listen((_) {
       modals['mapWindow'].open();
     });
+
+
+
+
 
     // SETTINGS WINDOW LISTENERS //
     ui.settingsButton.onClick.listen((_) {
@@ -36,6 +43,52 @@ class WindowManager {
       tab.classes.add('active');
       tabView.hidden = false;
 
+    });
+
+    //listen for onChange events so that clicking the label or the checkbox will call this method
+    querySelectorAll('.ChatSettingsCheckbox').onChange.listen((Event event)
+    {
+      CheckboxInputElement checkbox = event.target as CheckboxInputElement;
+      if(checkbox.id == "ShowJoinMessages")
+        setJoinMessagesVisibility(checkbox.checked);
+      if(checkbox.id == "PlayMentionSound")
+        setPlayMentionSound(checkbox.checked);
+    });
+
+        //setup saved variables
+    if(localStorage["showJoinMessages"] != null)
+    {
+      //ugly because there is no method to parse bool from string in dart?
+      if(localStorage["showJoinMessages"] == "true")
+        setJoinMessagesVisibility(true);
+      else
+        setJoinMessagesVisibility(false);
+    }
+    else
+    {
+      localStorage["showJoinMessages"] = "false";
+      setJoinMessagesVisibility(false);
+    }
+    querySelectorAll("#ShowJoinMessages").forEach((Element element)
+    {
+      (element as CheckboxInputElement).checked = getJoinMessagesVisibility();
+    });
+    
+    if(localStorage["playMentionSound"] != null)
+    {
+      if(localStorage["playMentionSound"] == "true")
+        setPlayMentionSound(true);
+      else
+        setPlayMentionSound(false);
+    }
+    else
+    {
+      localStorage["playMentionSound"] = "true";
+      setJoinMessagesVisibility(true);
+    }
+    querySelectorAll("#PlayMentionSound").forEach((Element element)
+    {
+      (element as CheckboxInputElement).checked = getPlayMentionSound();
     });
 
 
@@ -71,6 +124,32 @@ class WindowManager {
     });
 
   }
+
+      /**
+       * Determines if messages like "<user> has joined" are shown to the player.
+       * 
+       * Sets the visibility of join messages to [visible]
+       */
+      void setJoinMessagesVisibility(bool visible)
+      {
+        _showJoinMessages = visible;
+        localStorage["showJoinMessages"] = visible.toString();
+      }
+      
+      /**
+       * Returns the visibility of messages like "<user> has joined"
+       */
+      bool getJoinMessagesVisibility() => _showJoinMessages;
+      
+      void setPlayMentionSound(bool enabled)
+      {
+        _playMentionSound = enabled;
+        localStorage["playMentionSound"] = enabled.toString();
+      }
+      
+      bool getPlayMentionSound() => _playMentionSound;
+
+  
 }
 
 /// A Dart interface to an html Modal
@@ -129,5 +208,6 @@ class Modal {
       dragging = false;
     });
   }
+
 
 }
