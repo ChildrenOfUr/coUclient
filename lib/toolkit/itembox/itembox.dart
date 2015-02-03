dd3e98a90218063b368e9d9673b98fe019bfa229
ppvk <paul.vankeuren@gmail.com>
2015-02-03 16:46:52 -0600

    Drag and Drop now lets you add stacks of items and shift items from box to box, fixed a meter issue where the mood percent had some decimal places

--- lib/toolkit/itembox/itembox.dart
+++ lib/toolkit/itembox/itembox.dart
@@ -11,40 +11,62 @@ import 'package:polymer/polymer.dart'; //for dragging items into vendor interfac
 class ItemBox extends PolymerElement {
   @published String item = '';
   @published int quantity = 0;
-  
+
+  Draggable dragItem;
+  Dropzone dropBox;
+
   ItemBox.created() : super.created() {
     this.attributes.putIfAbsent('item', () => '');
-    
+    updateQuantityOpacity();
+    initDragDrop();
     this.changes.listen((_) {
-      if (quantity <= 1)
+      updateQuantityOpacity();
+      if (quantity <= 1) 
         shadowRoot.querySelector('#item-quantity').hidden = true;
-      else
+      else 
         shadowRoot.querySelector('#item-quantity').hidden = false;
-    });
-    
-    
-    Draggable dragItem = new Draggable(this,
-        avatarHandler: new AvatarHandler.clone());
-    Dropzone dropBox = new Dropzone(this);
-    
-    dragItem.onDrag.listen((_) {
-      dragItem.avatarHandler.avatar.shadowRoot.querySelector('ur-panel').style
-        ..backgroundColor = 'transparent'
-        ..border = 'none';
-    });    
-    dropBox.onDrop.listen((d) {      
-      if (d.draggableElement.attributes['item'].isEmpty) return;
-      if (d.dropzoneElement.attributes['item'].isNotEmpty) {
-        if (d.dropzoneElement.item == d.draggableElement.item)
-          d.dropzoneElement.quantity += d.draggableElement.quantity;
-        else
-          return;        
-      };
-      d.dropzoneElement.item = d.draggableElement.item;
-      d.dropzoneElement.quantity = d.draggableElement.quantity;
-      d.draggableElement.item = '';
-      d.draggableElement.quantity = 0;
+      //resetDragDrop();
+      //updateDragDrop();
     });
   }
-  
+
+  updateQuantityOpacity() {
+    if (quantity <= 1) 
+      shadowRoot.querySelector('#item-quantity').hidden = true;
+    else 
+      shadowRoot.querySelector('#item-quantity').hidden = false;
+  }
+
+
+  initDragDrop() {
+    dragItem = new Draggable(this, avatarHandler: new AvatarHandler.clone());
+    dropBox = new Dropzone(this);
+
+    // Hide the 'panel' element of our dragged object
+    dragItem.onDrag.listen((_) {
+      dragItem.avatarHandler.avatar.shadowRoot.querySelector('ur-panel').style
+          ..backgroundColor = 'transparent'
+          ..border = 'none';
+    });
+
+
+    dropBox.onDrop.listen((d) {
+      // Don't drag nothingness
+      if (d.draggableElement.attributes['item'] == '') return;
+
+      // Add to similar item stacks, but don't add to different stacks
+      if (d.dropzoneElement.attributes['item'] != '') {
+        if (d.dropzoneElement.item == d.draggableElement.item) {
+          d.draggableElement.attributes['quantity'] = (d.draggableElement.quantity + d.dropzoneElement.quantity).toString();
+        } else return;
+      }
+      ;
+
+      d.dropzoneElement.attributes['item'] = d.draggableElement.attributes['item'];
+      d.dropzoneElement.attributes['quantity'] = d.draggableElement.attributes['quantity'];
+      d.draggableElement.attributes['item'] = '';
+      d.draggableElement.attributes['quantity'] = 0.toString();
+    });
+  }
+
 }
