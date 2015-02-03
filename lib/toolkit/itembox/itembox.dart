1b509caaf5988ef4cdff4a6ed70cd3916dfb15e0
ppvk <paul.vankeuren@gmail.com>
2015-02-03 13:31:23 -0600

    drag and drop functionality is solid on our itemboxes, experience the joy of dragging around potatoes and cherries.

--- lib/toolkit/itembox/itembox.dart
+++ lib/toolkit/itembox/itembox.dart
@@ -1,24 +1,33 @@
 library itembox;
 
-import 'package:polymer/polymer.dart';
 import 'dart:html';
-import 'package:dnd/dnd.dart'; //for dragging items into vendor interface
+
+import 'package:dnd/dnd.dart';
+import 'package:polymer/polymer.dart'; //for dragging items into vendor interface
 
 @CustomTag('ur-itembox')
 class ItemBox extends PolymerElement {
+  @published String item = '';
+  @published int quantity = 0;
+
   ItemBox.created() : super.created() {
+    this.attributes.putIfAbsent('item', () => '');
+    
     Draggable dragItem = new Draggable(this,
         avatarHandler: new AvatarHandler.clone());
     Dropzone dropBox = new Dropzone(this);
     
-    dropBox.onDrop.listen((d) {
-      //if (d.dropzoneElement.attributes['item'].trim() != '') return;
-      
+    dragItem.onDrag.listen((_) {
+      dragItem.avatarHandler.avatar.shadowRoot.querySelector('ur-panel').style
+        ..backgroundColor = 'transparent'
+        ..border = 'none';
+    });
+    
+    dropBox.onDrop.listen((d) {      
+      if (d.draggableElement.attributes['item'].isEmpty) return;
+      if (d.dropzoneElement.attributes['item'].isNotEmpty) return;
       d.dropzoneElement.attributes['item'] = d.draggableElement.attributes['item'];
       d.draggableElement.attributes['item'] = '';
     });
   }
-
-
-  @published String item = '';
 }
