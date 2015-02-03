70c06e177959179e2b3bac7cd4dd983114cb7e62
ppvk <paul.vankeuren@gmail.com>
2015-02-03 15:04:34 -0600

    dragging is a bit of a mess, but quantites are now displayed.

--- lib/toolkit/itembox/itembox.dart
+++ lib/toolkit/itembox/itembox.dart
@@ -5,13 +5,23 @@ import 'dart:html';
 import 'package:dnd/dnd.dart';
 import 'package:polymer/polymer.dart'; //for dragging items into vendor interface
 
+
+
 @CustomTag('ur-itembox')
 class ItemBox extends PolymerElement {
   @published String item = '';
   @published int quantity = 0;
-
+  
   ItemBox.created() : super.created() {
     this.attributes.putIfAbsent('item', () => '');
+    
+    this.changes.listen((_) {
+      if (quantity <= 1)
+        shadowRoot.querySelector('#item-quantity').hidden = true;
+      else
+        shadowRoot.querySelector('#item-quantity').hidden = false;
+    });
+    
     
     Draggable dragItem = new Draggable(this,
         avatarHandler: new AvatarHandler.clone());
@@ -21,13 +31,20 @@ class ItemBox extends PolymerElement {
       dragItem.avatarHandler.avatar.shadowRoot.querySelector('ur-panel').style
         ..backgroundColor = 'transparent'
         ..border = 'none';
-    });
-    
+    });    
     dropBox.onDrop.listen((d) {      
       if (d.draggableElement.attributes['item'].isEmpty) return;
-      if (d.dropzoneElement.attributes['item'].isNotEmpty) return;
-      d.dropzoneElement.attributes['item'] = d.draggableElement.attributes['item'];
-      d.draggableElement.attributes['item'] = '';
+      if (d.dropzoneElement.attributes['item'].isNotEmpty) {
+        if (d.dropzoneElement.item == d.draggableElement.item)
+          d.dropzoneElement.quantity += d.draggableElement.quantity;
+        else
+          return;        
+      };
+      d.dropzoneElement.item = d.draggableElement.item;
+      d.dropzoneElement.quantity = d.draggableElement.quantity;
+      d.draggableElement.item = '';
+      d.draggableElement.quantity = 0;
     });
   }
+  
 }
