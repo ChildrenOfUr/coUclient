91ff698e7015927a8601a47091b7c99a158dcb2e
ppvk <paul.vankeuren@gmail.com>
2015-02-03 10:49:29 -0600

    meters now update properly when their attributes change, added a 'debug' attribute, when 'true' the meters change values randomly to show off animations.

--- lib/components/meters/meters.dart
+++ lib/components/meters/meters.dart
@@ -3,11 +3,25 @@ library meters;
 import 'package:polymer/polymer.dart';
 import 'package:intl/intl.dart';
 import 'dart:html';
+import 'dart:async';
+import 'dart:math' show Random;
 
 @CustomTag('ur-meters')
 class Meters extends PolymerElement {
   Meters.created() : super.created() {
     update();
+    changes.listen( (_) {
+      update();
+    });
+    if (debug == true) {
+      Random r = new Random();
+      new Timer.periodic(new Duration(seconds:1), (_) {
+        energy = r.nextInt(maxenergy);
+        mood = r.nextInt(maxmood);
+        imagination = r.nextInt(999999);
+      
+      });
+    }
   }
  
   @published String playername;
@@ -15,13 +29,14 @@ class Meters extends PolymerElement {
   @published int maxmood;
   @published int energy;
   @published int maxenergy;
-
   @published int imagination;  
   
   NumberFormat commaFormatter = new NumberFormat("#,###");  
 
+  @published bool debug;
+  
   update() {
-    // update energy disk angles
+    // update energy disk angles and opacities
     shadowRoot.querySelector('#energyDisks .green')
       ..style.transform = 'rotate(${120 - (energy / maxenergy) * 120}deg)';
     shadowRoot.querySelector('#energyDisks .red')
