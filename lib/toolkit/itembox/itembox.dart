bcafd629ed7dce87f4fe1fb131ed6373520acd4f
Robert McDermot <robert.mcdermot@gmail.com>
2015-02-04 18:17:53 -0500

    implemented item swapping for ur-item box (i.e. directly swap positions of the cherries and the potatoes in the demo)

--- lib/toolkit/itembox/itembox.dart
+++ lib/toolkit/itembox/itembox.dart
@@ -1,68 +1,66 @@
 library itembox;
 
-import 'dart:html';
-
 import 'package:dnd/dnd.dart';
 import 'package:polymer/polymer.dart';
 
 @CustomTag('ur-itembox')
-class ItemBox extends PolymerElement {
-  @published String item = '';
-  @published int quantity = 0;
+class ItemBox extends PolymerElement
+{
+	@published String item = '';
+	@published int quantity = 0;
 
-  Draggable _dragItem;
-  Dropzone _dropBox;
+	Draggable _dragItem;
+	Dropzone _dropBox;
 
-  ItemBox.created() : super.created() {
-    this.attributes.putIfAbsent('item', () => '');
-    updateQuantityOpacity();
-    initDragDrop();
-    this.changes.listen((_) {
-      updateQuantityOpacity();
-      if (quantity <= 1) 
-        shadowRoot.querySelector('#item-quantity').hidden = true;
-      else 
-        shadowRoot.querySelector('#item-quantity').hidden = false;
-    });
-  }
+	ItemBox.created() : super.created()
+	{
+		this.attributes.putIfAbsent('item', () => '');
+		initDragDrop();
+	}
 
-  updateQuantityOpacity() {
-    if (quantity <= 1) 
-      shadowRoot.querySelector('#item-quantity').hidden = true;
-    else 
-      shadowRoot.querySelector('#item-quantity').hidden = false;
-  }
+	initDragDrop()
+	{
+		_dragItem = new Draggable(this, avatarHandler: new AvatarHandler.clone());
+		_dropBox = new Dropzone(this);
 
+		// Hide the 'panel' element of our dragged object
+		_dragItem.onDrag.listen((_)
+		{
+			_dragItem.avatarHandler.avatar.shadowRoot.querySelector('ur-panel').style
+				..backgroundColor = 'transparent'
+				..border = 'none';
+		});
 
-  initDragDrop() {
-    _dragItem = new Draggable(this, avatarHandler: new AvatarHandler.clone());
-    _dropBox = new Dropzone(this);
+		_dropBox.onDrop.listen((d)
+		{
+			// Don't drag nothingness
+			if (d.draggableElement.item == '')
+				return;
 
-    // Hide the 'panel' element of our dragged object
-    _dragItem.onDrag.listen((_) {
-      _dragItem.avatarHandler.avatar.shadowRoot.querySelector('ur-panel').style
-          ..backgroundColor = 'transparent'
-          ..border = 'none';
-    });
+			// Add to similar item stacks, but don't add to different stacks
+			if (d.dropzoneElement.item != '')
+			{
+				if (d.dropzoneElement.item == d.draggableElement.item)
+				{
+					int total = d.draggableElement.quantity + d.dropzoneElement.quantity;
+					d.draggableElement.attributes['quantity'] = total.toString();
+				}
+				else
+				{
+					//since this swaps all the attributes of each element
+					//it effectively swaps the elements
+					Map<String,String> temp = {};
+					d.draggableElement.attributes.forEach((key,value) => temp[key] = value);
+					d.draggableElement.attributes = d.dropzoneElement.attributes;
+					d.dropzoneElement.attributes = temp;
+					return;
+				}
+			}
 
-
-    _dropBox.onDrop.listen((d) {
-      // Don't drag nothingness
-      if (d.draggableElement.attributes['item'] == '') return;
-
-      // Add to similar item stacks, but don't add to different stacks
-      if (d.dropzoneElement.attributes['item'] != '') {
-        if (d.dropzoneElement.item == d.draggableElement.item) {
-          d.draggableElement.attributes['quantity'] = (d.draggableElement.quantity + d.dropzoneElement.quantity).toString();
-        } else return;
-      }
-      ;
-
-      d.dropzoneElement.attributes['item'] = d.draggableElement.attributes['item'];
-      d.dropzoneElement.attributes['quantity'] = d.draggableElement.attributes['quantity'];
-      d.draggableElement.attributes['item'] = '';
-      d.draggableElement.attributes['quantity'] = 0.toString();
-    });
-  }
-
+			d.dropzoneElement.attributes['item'] = d.draggableElement.attributes['item'];
+			d.dropzoneElement.attributes['quantity'] = d.draggableElement.attributes['quantity'];
+			d.draggableElement.attributes['item'] = '';
+			d.draggableElement.attributes['quantity'] = 0.toString();
+		});
+	}
 }
