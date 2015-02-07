f61beb8660b4542e797a84797213837e507a8924
ppvk <paul.vankeuren@gmail.com>
2015-02-07 16:00:14 -0600

    added empty webcomponent for the login dialog

---
+++ lib/components/login/login.dart
@@ -0,0 +1,13 @@
+library inventory;
+
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+
+@CustomTag('ur-login')
+class UrLogin extends PolymerElement {
+  
+  UrLogin.created() : super.created() {
+
+  }
+
+}
