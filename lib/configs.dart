0a8d4c94a34f1816cb1fda828f8f35866a40a390
Robert McDermot <robert.mcdermot@gmail.com>
2015-03-21 15:11:50 -0400

    debugging message

--- lib/configs.dart
+++ lib/configs.dart
@@ -11,6 +11,7 @@ class Configs
     static Future init() async
     {
     	baseAddress = await HttpRequest.getString('server_domain.txt');
+    	baseAddress.trim();
     	utilServerAddress = '$baseAddress:8181';
         websocketServerAddress = '$baseAddress:8282';
         authAddress = '$baseAddress:8383';
