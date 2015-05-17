9e75fe568a64c6955ae7baad604725ef4d7eb5f6
Robert McDermot <robert.mcdermot@gmail.com>
2015-05-17 16:07:47 -0400

    fixed login styling, rearranged keyboard settings, made map key toggle the window instead of just open

--- lib/src/display/windows/map_window.dart
+++ lib/src/display/windows/map_window.dart
@@ -1,24 +1,28 @@
 part of couclient;
 
 class MapWindow extends Modal {
-  String id = 'mapWindow';
+	String id = 'mapWindow';
 
-  MapWindow() {
+	MapWindow() {
 
-    prepare();
-    // MAPWINDOW LISTENERS //
-    view.mapButton.onClick.listen((_) {
-      this.open();
-      worldMap = new WorldMap(currentStreet.hub_id);
-    });
+		prepare();
+		// MAPWINDOW LISTENERS //
+		view.mapButton.onClick.listen((_) {
+			this.open();
+			worldMap = new WorldMap(currentStreet.hub_id);
+		});
 
-    document.onKeyDown.listen((KeyboardEvent k) {
-    	if (k.keyCode == inputManager.keys["MapBinding"] && !inputManager.ignoreKeys) {
-    		this.open();
-    		worldMap = new WorldMap(currentStreet.hub_id);
-    	}
-    });
-
-  }
-
+		document.onKeyDown.listen((KeyboardEvent k) {
+			if(k.keyCode == inputManager.keys["MapBindingPrimary"]
+			   || k.keyCode == inputManager.keys["MapBindingAlt"]
+			      && !inputManager.ignoreKeys) {
+				if(this.window.hidden) {
+					this.open();
+					worldMap = new WorldMap(currentStreet.hub_id);
+				} else {
+					this.close();
+				}
+			}
+		});
+	}
 }
