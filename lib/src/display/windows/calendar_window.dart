c309e15ac808e9f23c1131855c2338890543e0a5
klikini <andyccastille@gmail.com>
2015-05-25 14:48:49 -0500

    calendar has real time

--- lib/src/display/windows/calendar_window.dart
+++ lib/src/display/windows/calendar_window.dart
@@ -5,6 +5,12 @@ class CalendarWindow extends Modal {
 
   CalendarWindow() {
     prepare();
+
+    new Service([#timeUpdate], (Message event) {
+        querySelector("#calCurrDay").text = clock.dayofweek;
+        querySelector("#calCurrTime").text = clock.time;
+        querySelector("#calCurrDate").text = clock.day + ' of ' + clock.month;
+    });
     
     querySelector("#time").onClick.listen((_) {
       this.open();
