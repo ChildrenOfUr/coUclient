82e5580d8946b773c3360999a01e6976a60bd7e6
Robert McDermot <robert.mcdermot@gmail.com>
2015-06-13 21:45:34 -0400

    raindrops

--- lib/src/systems/weather.dart
+++ lib/src/systems/weather.dart
@@ -18,11 +18,18 @@ class WeatherManager {
 			try {
 				String value = localStorage["WeatherEffectsIntensity"];
 				_intensity = WeatherIntensity.values[int.parse(value)];
-			} catch(err){}
+			} catch(err) {
+			}
 
 			if(localStorage["WeatherEffectsEnabled"] == "false") {
 				_enabled = false;
+			} else if(localStorage["WeatherEffectsEnabled"] == "true") {
+				_createRain();
 			}
+
+			//need a service to listen to time events and respond by coloring the
+			//weather overlay as needed (possibly change the background gradient?
+			new Service([#timeUpdate],_changeAmbientColor);
 		}
 	}
 
@@ -34,12 +41,65 @@ class WeatherManager {
 		return _weatherManager;
 	}
 
+	static void _changeAmbientColor(Message m) {
+		List<dynamic> timePieces = m.content;
+		String time = timePieces[0];
+		bool am = time.contains('am');
+		List<String> hourmin = time.substring(0,time.length-2).split(':');
+		int hour = int.parse(hourmin[0]);
+		int minute = int.parse(hourmin[1]);
+		List<num> rgba = [255,255,255,0];
+
+		if(!am) {
+			if(hour >= 6 && hour <= 9) {
+				//go towards rgba(218,150,45,.15) from 255,255,255,0
+				//there's 180 minutes, what minute are we at
+				currentMinute = (9-hour)*60+minute;
+				num percent = currentMinute/180;
+				rgba = _tweenColor([255,255,255,0],[218,150,45,.15],percent);
+			} else if(hour >= 9){
+				//go towards rgba(17,17,47,.5) from 218,150,45,.15
+				currentMinute = (12-hour)*60+minute;
+				num percent = currentMinute/180;
+				rgba = _tweenColor([218,150,45.15],[17,17,47,.5],percent);
+			}
+		} else {
+			if(hour < 5) {
+				rgba = [17,17,47,.5];
+			}else if(hour >= 5 && hour <= 7) {
+				currentMinute = (7-hour)*60+minute;
+				num percent = currentMinute/180;
+				rgba = _tweenColor([17,17,47,.5],[218,150,45,.15],percent);
+			} else if(hour >= 7 && hour < 9) {
+				currentMinute = (9-hour)*60+minute;
+				num percent = currentMinute/180;
+				rgba = _tweenColor([218,150,45,.15],[255,255,255,0],percent);
+			}
+		}
+
+		_weatherLayer.style.backgroundColor = 'rgba(${rgba[0]},${rgba[1]},${rgba[2]},${rgba[3]})';
+	}
+
+	static List<num> _tweenColor(List<num> start, List<num> end, num percent) {
+		num r = percent*((start[0]-end[0]).abs());
+		num g = percent*((start[1]-end[1]).abs());
+		num b = percent*((start[2]-end[2]).abs());
+		num a = percent*((start[3]-end[3]).abs());
+		return [r,g,b,a];
+	}
+
 	// function to generate drops
-	static void _createRain({WeatherIntensity intensity:WeatherIntensity.MEDIUM}) {
+	static void _createRain() {
 		if(!_enabled) {
 			return;
 		}
 		log('raining: $_intensity');
+
+		//set the sky to cloudy
+		if(!_weatherLayer.classes.contains('cloudy')) {
+			_weatherLayer.classes.add('cloudy');
+		}
+
 		Random random = new Random();
 		int numDrops = (500 * ((intensity.index + 1) / WeatherIntensity.values.length)).toInt();
 
@@ -49,6 +109,8 @@ class WeatherManager {
 
 			DivElement raindrop = new DivElement()
 				..className = 'drop'
+				..style.width = '${random.nextInt(2)+1}px'
+				..style.height = '${random.nextInt(30)+20}px'
 				..style.left = '${dropLeft}px'
 				..style.top = '${dropTop}px';
 
@@ -59,6 +121,7 @@ class WeatherManager {
 	//clear rain from screen
 	static void clearRain() {
 		_weatherLayer.children.clear();
+		_weatherLayer.classes.remove('cloudy');
 	}
 
 	static bool get enabled => _enabled;
@@ -68,7 +131,7 @@ class WeatherManager {
 		_enabled = enabled;
 		clearRain();
 		if(_enabled) {
-			_createRain(intensity:_intensity);
+			_createRain();
 		}
 	}
 
@@ -79,7 +142,7 @@ class WeatherManager {
 		localStorage["WeatherEffectsIntensity"] = _intensity.index.toString();
 		clearRain();
 		if(enabled) {
-			_createRain(intensity:_intensity);
+			_createRain();
 		}
 	}
 }
