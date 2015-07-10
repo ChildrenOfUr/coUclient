9a7d138fc084719498f5f98211a07d6f9534f154
Robert McDermot <robert.mcdermot@gmail.com>
2015-07-09 22:12:38 -0400

    debug messages

--- domain_checker.sh
+++ domain_checker.sh
@@ -2,8 +2,10 @@ branch="$(git rev-parse --abbrev-ref HEAD)"
 if [ "$branch" == "master" ]
 then
     echo "server.childrenofur.com" > web/server_domain.txt
+    echo "branch=$branch"
     echo "server domain will be server.childrenofur.com"
 else
     echo "robertmcdermot.com" > web/server_domain.txt
+    echo "branch=$branch"
     echo "server domain will be robertmcdermot.com"
 fi
