a7b5b6893bd2676de6c535ce503dde06d4beaee7
klikini <klikini@robiotic.net>
2015-07-28 16:58:40 -0500

    report images

--- lib/src/display/windows/bug_window.dart
+++ lib/src/display/windows/bug_window.dart
@@ -24,9 +24,9 @@ class BugWindow extends Modal {
     // TODO someday this should be serverside. Let's not give our keys to the client unless we have to.
     w.querySelector('ur-button').onClick.listen((_) async {
       // send to slack
-//			slack.Slack s = new slack.Slack(SLACK_BUG_WEBHOOK);
-//			slack.Message m = new slack.Message('${view.bugReportMeta.text}\n\nReport Type: ${view.bugReportType.value}\n\nComments: ${input.value}\n\nEmail: ${game.email}\n', username:game.username);
-//			s.send(m);
+      slack.Slack s = new slack.Slack(SLACK_BUG_WEBHOOK);
+      slack.Message m = new slack.Message('${view.bugReportMeta.text}\n\nReport Type: ${view.bugReportType.value}\n\nComments: ${input.value}\n\nEmail: ${game.email}\n', username:game.username);
+      s.send(m);
       // send to server
       FormData data = new FormData()
         ..append("title", view.bugReportTitle.value)
@@ -37,16 +37,14 @@ class BugWindow extends Modal {
         ..append("email", game.email)
         ..append("category", view.bugReportType.value);
       if (view.bugReportImage.files.isNotEmpty) {
-        data.appendBlob("image", view.bugReportImage.files.first, view.bugReportImage.files.first.name);
-      } else {
-        data.append("image", "");
+        data.appendBlob("image", view.bugReportImage.files.first);
       }
       await HttpRequest.request("http://${Configs.utilServerAddress}/report/add", method: "POST", sendData: data);
       // complete
       w.hidden = true;
       view.bugReportTitle.value = "";
       input.value = "";
-      view.bugReportImage.files.clear();
+      (view.bugReportImage.parent as FormElement).reset();
     });
   }
 
