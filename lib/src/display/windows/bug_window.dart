f26a7196c47cd5682da41d69a85c36826c64fd62
klikini <klikini@robiotic.net>
2015-08-03 14:17:20 -0500

    require report title

--- lib/src/display/windows/bug_window.dart
+++ lib/src/display/windows/bug_window.dart
@@ -23,28 +23,30 @@ class BugWindow extends Modal {
     // Submits the Bug
     // TODO someday this should be serverside. Let's not give our keys to the client unless we have to.
     w.querySelector('ur-button').onClick.listen((_) async {
-      // send to slack
-      slack.Slack s = new slack.Slack(SLACK_BUG_WEBHOOK);
-      slack.Message m = new slack.Message('${view.bugReportMeta.text}\n\nReport Type: ${view.bugReportType.value}\n\nComments: ${input.value}\n\nEmail: ${game.email}\n', username:game.username);
-      s.send(m);
-      // send to server
-      FormData data = new FormData()
-        ..append("title", view.bugReportTitle.value)
-        ..append("description", input.value)
-        ..append("log", bugLog)
-        ..append("useragent", window.navigator.userAgent)
-        ..append("username", game.username)
-        ..append("email", game.email)
-        ..append("category", view.bugReportType.value);
-      if (view.bugReportImage.files.isNotEmpty) {
-        data.appendBlob("image", view.bugReportImage.files.first);
+      if (view.bugReportTitle.value.trim() != "") {
+        // send to slack
+        slack.Slack s = new slack.Slack(SLACK_BUG_WEBHOOK);
+        slack.Message m = new slack.Message('${view.bugReportMeta.text}\n\nReport Type: ${view.bugReportType.value}\n\nComments: ${input.value}\n\nEmail: ${game.email}\n', username:game.username);
+        s.send(m);
+        // send to server
+        FormData data = new FormData()
+          ..append("title", view.bugReportTitle.value)
+          ..append("description", input.value)
+          ..append("log", bugLog)
+          ..append("useragent", window.navigator.userAgent)
+          ..append("username", game.username)
+          ..append("email", game.email)
+          ..append("category", view.bugReportType.value);
+        if (view.bugReportImage.files.isNotEmpty) {
+          data.appendBlob("image", view.bugReportImage.files.first);
+        }
+        await HttpRequest.request("http://${Configs.utilServerAddress}/report/add", method: "POST", sendData: data);
+        // complete
+        w.hidden = true;
+        view.bugReportTitle.value = "";
+        input.value = "";
+        (view.bugReportImage.parent as FormElement).reset();
       }
-      await HttpRequest.request("http://${Configs.utilServerAddress}/report/add", method: "POST", sendData: data);
-      // complete
-      w.hidden = true;
-      view.bugReportTitle.value = "";
-      input.value = "";
-      (view.bugReportImage.parent as FormElement).reset();
     });
   }
 
