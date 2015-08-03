9462ea02b9b94974bc3e0d8569d51fdb847f0635
klikini <klikini@robiotic.net>
2015-08-02 22:12:51 -0500

    autoformat to push

--- lib/src/display/render/camera.dart
+++ lib/src/display/render/camera.dart
@@ -2,35 +2,31 @@ part of couclient;
 
 Camera camera = new Camera();
 
-class Camera
-{
-	int _x = 0, _y = 0, zoom = 0;
+class Camera {
+  int _x = 0, _y = 0, zoom = 0;
 
-	// for future eyeballery
-	bool dirty = true;
-	MutableRectangle visibleRect;
+  // for future eyeballery
+  bool dirty = true;
+  MutableRectangle visibleRect;
 
-	void setCameraPosition(int newX, int newY)
-	{
-		if(newX != _x || newY != _y)
-			dirty = true;
-		_x = newX;
-		_y = newY;
+  void setCameraPosition(int newX, int newY) {
+    if (newX != _x || newY != _y)
+      dirty = true;
+    _x = newX;
+    _y = newY;
 
-		if(visibleRect == null)
-		{
-			visibleRect = new MutableRectangle(_x, _y, view.worldElementWidth, view.worldElementHeight);
-		}
-		else
-		{
-			visibleRect.left = _x;
-			visibleRect.top = _y;
-			visibleRect.width = view.worldElementWidth;
-			visibleRect.height = view.worldElementHeight;
-		}
-	}
+    if (visibleRect == null) {
+      visibleRect = new MutableRectangle(_x, _y, view.worldElementWidth, view.worldElementHeight);
+    }
+    else {
+      visibleRect.left = _x;
+      visibleRect.top = _y;
+      visibleRect.width = view.worldElementWidth;
+      visibleRect.height = view.worldElementHeight;
+    }
+  }
 
-	int getX() => _x;
+  int getX() => _x;
 
-	int getY() => _y;
+  int getY() => _y;
 }
