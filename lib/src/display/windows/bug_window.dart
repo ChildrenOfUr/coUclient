a5e2605d5072084b4eb9810f6ab99085dabe254a
klikini <klikini@robiotic.net>
2015-10-10 17:58:34 -0500

    (temporarily?) break the bug window

--- lib/src/display/windows/bug_window.dart
+++ lib/src/display/windows/bug_window.dart
@@ -2,52 +2,30 @@ part of couclient;
 
 class BugWindow extends Modal {
   String id = 'bugWindow';
+  String messagesLogged = "";
   Service debugService;
-  String bugLog = "";
-  bool sending = false;
+  Element component = querySelector("user-feedback");
 
   BugWindow() {
     debugService = new Service(['debug'], logMessage);
 
     prepare();
-    String headerDeco = "/////////////"; // prime number of forward slashes
-    view.bugReportMeta.text = headerDeco + ' USER AGENT ' + headerDeco + '\n' + window.navigator.userAgent + '\n' + headerDeco + ' CLIENT LOG ' + headerDeco;
+	setupUiButton(view.bugButton, openCallback: open());
 
-    setupUiButton(view.bugButton, openCallback:_prepareReport);
+	new Service(["gameLoaded"], (_) {
+		component.attributes
+			..["username"] = game.username
+			..["useragent"] = browser.toString() + "\n" + window.navigator.userAgent
+			..["log"] = messagesLogged;
+	});
+
+	new Service(["REPORT_SENT"], (Map data) {
+		close();
+		toast("Report, uh, reported!");
+	});
   }
 
-  void _prepareReport() {
-    Element w = this.displayElement;
-    TextAreaElement input = w.querySelector('textarea');
-    input.value = '';
-
-    // Submits the Bug
-    w.querySelector('ur-button').onClick.listen((_) async {
-      if (!sending) {
-        sending = true;
-        if (view.bugReportTitle.value.trim() != "") {
-          // send to server
-          FormData data = new FormData()
-            ..append("token", rsToken)
-            ..append("title", view.bugReportTitle.value)
-            ..append("description", input.value)
-            ..append("log", bugLog)
-            ..append("useragent", window.navigator.userAgent)
-            ..append("username", game.username)
-            ..append("category", view.bugReportType.value);
-          await HttpRequest.request("http://${Configs.utilServerAddress}/report/add", method: "POST", sendData: data);
-          // complete
-          w.hidden = true;
-          view.bugReportTitle.value = "";
-          input.value = "";
-        }
-        sending = false;
-      }
-    });
-  }
-
-  logMessage(var message) {
-    bugLog += message + "\n";
-    view.bugReportMeta.text += message + "\n";
+  void logMessage(var message) {
+	  messagesLogged += message + "\n";
   }
 }
