82880f9609823cd86015d1cd687b1d6b8610f84f
klikini <klikini@robiotic.net>
2016-02-27 21:28:09 -0600

    format loop.dart

--- lib/src/display/loop.dart
+++ lib/src/display/loop.dart
@@ -2,44 +2,46 @@ part of couclient;
 
 double timeLast = 0.0;
 String lastXY = "";
+
 // Our gameloop
-update(double dt)
-{
-	if(serverDown) {
+update(double dt) {
+	if (serverDown) {
 		return;
 	}
 
 	CurrentPlayer.update(dt);
 
-	otherPlayers.forEach((String username, Player otherPlayer)
-	{
-		if(otherPlayer.currentAnimation != null)
+	otherPlayers.forEach((String username, Player otherPlayer) {
+		if (otherPlayer.currentAnimation != null) {
 			otherPlayer.currentAnimation.updateSourceRect(dt);
+		}
+
 		double x = otherPlayer.posX;
 		double y = otherPlayer.posY;
 		String transform = "translateY(${y}px) translateX(${x}px) translateZ(0)";
-		if(!otherPlayer.facingRight)
-		{
+
+		if (!otherPlayer.facingRight) {
 			transform += ' scale3d(-1,1,1)';
 			otherPlayer.playerParentElement.classes
 				..add("facing-left")
 				..remove("facing-right");
 			otherPlayer.playerName.style.transform = 'translateY(calc(-100% - 34px)) scale3d(-1,1,1)';
 
-			if(otherPlayer.chatBubble != null)
+			if (otherPlayer.chatBubble != null) {
 				otherPlayer.chatBubble.textElement.style.transform = 'scale3d(-1,1,1)';
-		}
-		else
-		{
+			}
+		} else {
 			transform += ' scale3d(1,1,1)';
 			otherPlayer.playerParentElement.classes
 				..add("facing-right")
 				..remove("facing-left");
 			otherPlayer.playerName.style.transform = 'translateY(calc(-100% - 34px)) scale3d(1,1,1)';
 
-			if(otherPlayer.chatBubble != null)
+			if (otherPlayer.chatBubble != null) {
 				otherPlayer.chatBubble.textElement.style.transform = 'scale3d(1,1,1)';
+			}
 		}
+
 		otherPlayer.playerParentElement.style.transform = transform;
 	});
 
@@ -48,13 +50,12 @@ update(double dt)
 
 	//update the other clients with our position & street
 	timeLast += dt;
-	if(timeLast > .03 && playerSocket != null && playerSocket.readyState == WebSocket.OPEN)
-	{
-		String xy = CurrentPlayer.posX.toString()+","+CurrentPlayer.posY.toString();
-		if(xy == lastXY) //don't send updates when the player doesn't move - except once every 5 seconds
-		{
-			if(timeLast < 5)
-				return;
+	if (timeLast > .03 && playerSocket != null && playerSocket.readyState == WebSocket.OPEN) {
+		String xy = CurrentPlayer.posX.toString() + "," + CurrentPlayer.posY.toString();
+
+		if (xy == lastXY && timeLast < 5) {
+			// Don't send updates when the player doesn't move - except once every 5 seconds
+			return;
 		}
 
 		lastXY = xy;
