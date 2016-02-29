911a1d8b0856462c61768e0d48241115879acd50
Andy Castille <klikini@robiotic.net>
2016-02-28 18:40:23 -0600

    fix something I broke in previous commit

--- lib/src/display/ui_templates/right_click_menu.dart
+++ lib/src/display/ui_templates/right_click_menu.dart
@@ -136,9 +136,6 @@ class RightClickMenu {
 
 			if (option["description"] != null) {
 				showActionError(tooltip, option["description"]);
-				menuitem.onClick.listen((_) {
-					toast(option["description"]);
-				});
 			}
 
 			wrapper.append(menuitem);
@@ -250,9 +247,10 @@ class RightClickMenu {
 					menuitem.dispatchEvent(new MouseEvent('click', clientX: Click.client.x, clientY: Click.client.y));
 				} else {
 					menuitem.click();
-					if (menuitem.classes.contains("RCItemDisabled")) {
-						toast((option[0] as String).split("|")[4]);
-					}
+				}
+
+				if (menuitem.classes.contains("RCItemDisabled")) {
+					toast((option[0] as String).split("|")[4]);
 				}
 			});
 
