b6af794f8984be0cda8018d63af3ffd3f98fd049
Andy Castille <klikini@robiotic.net>
2016-03-02 21:49:45 -0600

    more dark theming

--- lib/src/display/darkui.dart
+++ lib/src/display/darkui.dart
@@ -10,8 +10,10 @@ part of couclient;
 */
 
 abstract class DarkUI {
+	/// The class name to apply or attribute to set when in dark mode
 	static final String DARK_CLASS = "darkui";
 
+	/// Sets mode based on true (dark) or false (light)
 	static set darkMode(bool newMode) {
 		if (newMode) {
 			toDarkMode();
@@ -20,32 +22,46 @@ abstract class DarkUI {
 		}
 	}
 
+	/// Returns true if in dark mode, false if not
 	static bool get darkMode {
 		return document.body.classes.contains(DARK_CLASS);
 	}
 
+	/// Sets mode based on "light" or "dark" strings
 	static set modeName(String newMode) {
 		newMode = newMode.trim().toLowerCase();
 		darkMode = (newMode == "dark");
 	}
 
+	/// Returns the current mode as "light" or "dark"
 	static String get modeName {
 		return (darkMode ? "dark" : "light");
 	}
 
+	/// Returns all of the elements that should be styled
+	static List<Element> get appliedElements {
+		return new List()
+			..add(document.body)
+			..add(querySelector("ur-meters"))
+			..add(querySelector("ur-mailbox"))
+			..addAll(querySelectorAll("ur-button"));
+	}
+
+	/// Switches to dark mode
 	static void toDarkMode() {
-		document.body.classes.add(DARK_CLASS);
-		querySelector("ur-meters").attributes[DARK_CLASS] = "true";
-		querySelectorAll("ur-button").forEach((Element element) {
-			element.attributes[DARK_CLASS] = "true";
+		appliedElements.forEach((Element element) {
+			element
+				..classes.add(DARK_CLASS)
+				..attributes[DARK_CLASS] = "true";
 		});
 	}
 
+	/// Switches to light mode
 	static void toLightMode() {
-		document.body.classes.remove(DARK_CLASS);
-		querySelector("ur-meters").attributes[DARK_CLASS] = "false";
-		querySelectorAll("ur-button").forEach((Element element) {
-			element.attributes[DARK_CLASS] = "false";
+		appliedElements.forEach((Element element) {
+			element
+				..classes.remove(DARK_CLASS)
+				..attributes[DARK_CLASS] = "false";
 		});
 	}
 }
