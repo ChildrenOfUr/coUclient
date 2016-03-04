6467bfe53ff57797c8233bfed11fdfecc691820a
Andy Castille <klikini@robiotic.net>
2016-03-03 20:20:37 -0600

    apply dark styles to musicplayer

--- lib/src/display/darkui.dart
+++ lib/src/display/darkui.dart
@@ -21,6 +21,7 @@ abstract class DarkUI {
 			..add(querySelector("ur-meters /deep/ #playerName"))
 			..add(querySelector("ur-meters /deep/ #imaginationText"))
 			..add(querySelector("ur-mailbox /deep/ core-pages"))
+			..add(querySelector("ur-musicplayer /deep/ ur-panel"))
 			..addAll(querySelectorAll("ur-button"));
 	}
 
