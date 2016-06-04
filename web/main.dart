e42872a213480207925f34161a2d0b6983b79818
Andy Castille <klikini@robiotic.net>
2016-06-04 11:29:28 -0500

    organize main

--- web/main.dart
+++ web/main.dart
@@ -13,259 +13,172 @@ import 'dart:js';
 // (unused) // import 'dart:profiler';
 
 
-// POLYMER COMPONENTS //
-import 'package:polymer/polymer.dart';
-import 'package:couclient/components/mailbox/mailbox.dart';
-import 'package:cou_toolkit/toolkit/slider/slider.dart';
-import 'package:cou_login/login/login.dart';
-import 'package:paper_elements/paper_radio_group.dart';
-import 'package:paper_elements/paper_toggle_button.dart';
+// Polymer components
 
-import 'package:couclient/src/network/metabolics.dart';
-import 'package:couclient/src/network/server_interop/itemdef.dart';
+import 'package:cou_login/login/login.dart'; // Login widget
+import 'package:cou_toolkit/toolkit/slider/slider.dart'; // Volume sliders
+import 'package:couclient/components/mailbox/mailbox.dart'; // Mailbox UI
+import 'package:couclient/src/network/metabolics.dart'; // Metabolics display
+import 'package:paper_elements/paper_radio_group.dart'; // Radio buttons
+import 'package:paper_elements/paper_toggle_button.dart'; // Checkboxes
+import 'package:polymer/polymer.dart'; // Polymer
 
-// LIBRARIES //
-// Used for NumberFormat
-import 'package:intl/intl.dart';
+// Libraries
 
-// SoundCloud Helper
-import 'package:scproxy/scproxy.dart';
+import 'package:browser_detect/browser_detect.dart'; // Special browser errors
+import 'package:couclient/configs.dart'; // Global data
+import 'package:couclient/src/network/server_interop/itemdef.dart'; // Items
+import 'package:dnd/dnd.dart'; // Webaudio api
+import 'package:gorgon/gorgon.dart'; // Audio and graphics
+import 'package:intl/intl.dart'; // Used for NumberFormat
+import 'package:jsonx/jsonx.dart'; // Dart object <-> JSON conversions
+import 'package:libld/libld.dart'; // Asset loading
+import 'package:scproxy/scproxy.dart'; // SoundCloud helper
+import 'package:transmit/transmit.dart'; // Event bus
+import "package:xml/xml.dart" as XML; // Blog post checking
 
-// Audio and Graphics
-import 'package:gorgon/gorgon.dart';
-
-// for Webaudio api
-import 'package:dnd/dnd.dart';
-
-//for dragging items into vendor interface
-// Asset Loading
-import 'package:libld/libld.dart';
-
-// Nice and simple asset loading.
-// Event Bus and Pumps // for more infomation see '/doc/pumps.md'
-import 'package:transmit/transmit.dart';
-
-//converting JSON to Dart objects and back
-import 'package:jsonx/jsonx.dart';
-
-// Global data
-import 'package:couclient/configs.dart';
-
-// Special browser errors
-import 'package:browser_detect/browser_detect.dart';
-
-// Blog post checking
-import "package:xml/xml.dart" as XML;
+// Start Polymer
 
 export 'package:polymer/init.dart';
 
-// SYSTEMS MODULES //
-part 'package:couclient/src/systems/clock.dart';
-
-part 'package:couclient/src/systems/gps.dart';
-
-part 'package:couclient/src/systems/weather.dart';
-
-part 'package:couclient/src/systems/quest_manager.dart';
-
-part 'package:couclient/src/systems/commands.dart';
-
-part 'package:couclient/src/game/input.dart';
-
-part 'package:couclient/src/game/joystick.dart';
-
-part 'package:couclient/src/systems/util.dart';
+// Systems
 
 part 'package:couclient/src/display/gps_display.dart';
+part 'package:couclient/src/game/input.dart';
+part 'package:couclient/src/game/joystick.dart';
+part 'package:couclient/src/systems/clock.dart';
+part 'package:couclient/src/systems/commands.dart';
+part 'package:couclient/src/systems/gps.dart';
+part 'package:couclient/src/systems/quest_manager.dart';
+part 'package:couclient/src/systems/util.dart';
+part 'package:couclient/src/systems/weather.dart';
 
-// NETWORKING MODULES //
-part 'package:couclient/src/network/chat.dart';
-
-part 'package:couclient/src/network/streetservice.dart';
+// Networking
 
 part 'package:couclient/src/network/auth.dart';
-
-part 'package:couclient/src/network/server_interop/inventory.dart';
-
-part 'package:couclient/src/network/server_interop/so_chat.dart';
-
-part 'package:couclient/src/network/server_interop/so_item.dart';
-
-part 'package:couclient/src/network/server_interop/so_player.dart';
-
-part 'package:couclient/src/network/server_interop/so_street.dart';
-
-part 'package:couclient/src/network/server_interop/so_multiplayer.dart';
-
+part 'package:couclient/src/network/chat.dart';
 part 'package:couclient/src/network/item_action.dart';
-
 part 'package:couclient/src/network/metabolics_service.dart';
+part 'package:couclient/src/network/server_interop/inventory.dart';
+part 'package:couclient/src/network/server_interop/so_chat.dart';
+part 'package:couclient/src/network/server_interop/so_item.dart';
+part 'package:couclient/src/network/server_interop/so_multiplayer.dart';
+part 'package:couclient/src/network/server_interop/so_player.dart';
+part 'package:couclient/src/network/server_interop/so_street.dart';
+part 'package:couclient/src/network/streetservice.dart';
 
-// UI/UX MODULES //
-part 'package:couclient/src/display/view.dart';
-
-part 'package:couclient/src/display/chatpanel.dart';
-
-part 'package:couclient/src/display/chatmessage.dart';
-
-part 'package:couclient/src/display/meters.dart';
-
-part 'package:couclient/src/display/toast.dart';
+// UI
 
 part 'package:couclient/src/display/buff.dart';
-
-part 'package:couclient/src/systems/audio.dart';
-
-part 'package:couclient/src/display/render.dart';
-
-part 'package:couclient/src/display/loop.dart';
-
+part 'package:couclient/src/display/chatmessage.dart';
+part 'package:couclient/src/display/chatpanel.dart';
 part 'package:couclient/src/display/information_display.dart';
-
 part 'package:couclient/src/display/inv_dragging.dart';
-
+part 'package:couclient/src/display/loop.dart';
+part 'package:couclient/src/display/meters.dart';
+part 'package:couclient/src/display/minimap.dart';
+part 'package:couclient/src/display/render.dart';
+part 'package:couclient/src/display/toast.dart';
+part 'package:couclient/src/display/ui_templates/howmany.dart';
+part 'package:couclient/src/display/ui_templates/interactions_menu.dart';
+part 'package:couclient/src/display/ui_templates/item_chooser.dart';
+part 'package:couclient/src/display/ui_templates/menu_keys.dart';
+part 'package:couclient/src/display/ui_templates/right_click_menu.dart';
+part 'package:couclient/src/display/view.dart';
+part 'package:couclient/src/systems/audio.dart';
 part "package:couclient/src/display/blog_notifier.dart";
-
 part "package:couclient/src/display/darkui.dart";
-
 part "package:couclient/src/display/overlays/text_anim.dart";
 
-//  WINDOW MODULES //
-part 'package:couclient/src/display/windows/windows.dart';
-
-part 'package:couclient/src/display/windows/settings_window.dart';
-
-part 'package:couclient/src/display/windows/bag_window.dart';
-
-part 'package:couclient/src/display/windows/bug_window.dart';
-
-part 'package:couclient/src/display/windows/map_window.dart';
-
-part 'package:couclient/src/display/windows/motd_window.dart';
-
-part 'package:couclient/src/display/windows/vendor_window.dart';
-
-part 'package:couclient/src/display/windows/go_window.dart';
-
-part 'package:couclient/src/display/windows/calendar_window.dart';
-
-part 'package:couclient/src/display/windows/shrine_window.dart';
-
-part 'package:couclient/src/display/windows/rock_window.dart';
-
-part 'package:couclient/src/display/windows/item_window.dart';
-
-part 'package:couclient/src/display/windows/emoticon_picker.dart';
-
-part 'package:couclient/src/display/windows/useitem_window.dart';
-
-part 'package:couclient/src/display/windows/note_window.dart';
-
-part 'package:couclient/src/display/windows/questlog_window.dart';
+// Windows
 
 part 'package:couclient/src/display/windows/achievements_window.dart';
-
-part 'package:couclient/src/display/windows/quest_maker_window.dart';
-
+part 'package:couclient/src/display/windows/bag_window.dart';
+part 'package:couclient/src/display/windows/bug_window.dart';
+part 'package:couclient/src/display/windows/calendar_window.dart';
 part 'package:couclient/src/display/windows/change_username_window.dart';
+part 'package:couclient/src/display/windows/emoticon_picker.dart';
+part 'package:couclient/src/display/windows/go_window.dart';
+part 'package:couclient/src/display/windows/item_window.dart';
+part 'package:couclient/src/display/windows/map_window.dart';
+part 'package:couclient/src/display/windows/motd_window.dart';
+part 'package:couclient/src/display/windows/note_window.dart';
+part 'package:couclient/src/display/windows/preferences/preference.dart';
+part 'package:couclient/src/display/windows/preferences/preferences_window.dart';
+part 'package:couclient/src/display/windows/quest_maker_window.dart';
+part 'package:couclient/src/display/windows/questlog_window.dart';
+part 'package:couclient/src/display/windows/rock_window.dart';
+part 'package:couclient/src/display/windows/settings_window.dart';
+part 'package:couclient/src/display/windows/shrine_window.dart';
+part 'package:couclient/src/display/windows/useitem_window.dart';
+part 'package:couclient/src/display/windows/vendor_window.dart';
+part 'package:couclient/src/display/windows/windows.dart';
 
-// OVERLAYS //
-part 'package:couclient/src/display/overlays/overlay.dart';
-
-part 'package:couclient/src/display/overlays/newdayscreen.dart';
-
-part 'package:couclient/src/display/overlays/imgmenu.dart';
-
-part 'package:couclient/src/display/overlays/module_skills.dart';
-
-part 'package:couclient/src/display/overlays/levelup.dart';
+// Overlays
 
 part 'package:couclient/src/display/overlays/achievementget.dart';
+part 'package:couclient/src/display/overlays/imgmenu.dart';
+part 'package:couclient/src/display/overlays/levelup.dart';
+part 'package:couclient/src/display/overlays/module_skills.dart';
+part 'package:couclient/src/display/overlays/newdayscreen.dart';
+part 'package:couclient/src/display/overlays/overlay.dart';
 
-// WIDGET MODULES //
-part 'package:couclient/src/display/widgets/volumeslider.dart';
+// Widgets
 
 part 'package:couclient/src/display/widgets/soundcloud.dart';
+part 'package:couclient/src/display/widgets/volumeslider.dart';
 
-// STREET RENDERING MODULES //
+// Street rendering
+
 part 'package:couclient/src/display/render/camera.dart';
-
-part 'package:couclient/src/display/render/deco.dart';
-
-part 'package:couclient/src/display/render/ladder.dart';
-
-part 'package:couclient/src/display/render/wall.dart';
-
-part 'package:couclient/src/display/render/platform.dart';
-
-part 'package:couclient/src/display/render/signpost.dart';
-
 part 'package:couclient/src/display/render/collision_lines_debug.dart';
-
+part 'package:couclient/src/display/render/deco.dart';
+part 'package:couclient/src/display/render/ladder.dart';
+part 'package:couclient/src/display/render/maps_data.dart';
+part 'package:couclient/src/display/render/platform.dart';
+part 'package:couclient/src/display/render/signpost.dart';
+part 'package:couclient/src/display/render/wall.dart';
 part 'package:couclient/src/network/mapdata.dart';
 
-part 'package:couclient/src/display/render/maps_data.dart';
-
-// GAME MODULES //
-part 'package:couclient/src/game/game.dart';
-
-part 'package:couclient/src/game/entities/player.dart';
-
-part 'package:couclient/src/game/animation.dart';
-
-part 'package:couclient/src/game/chat_bubble.dart';
+// Game rendering
 
 part 'package:couclient/src/game/action_bubble.dart';
-
-part 'package:couclient/src/game/entities/entity.dart';
-
-part 'package:couclient/src/game/entities/wormhole.dart';
-
-part 'package:couclient/src/game/entities/npc.dart';
-
-part 'package:couclient/src/game/entities/plant.dart';
-
+part 'package:couclient/src/game/animation.dart';
+part 'package:couclient/src/game/chat_bubble.dart';
 part 'package:couclient/src/game/entities/door.dart';
-
+part 'package:couclient/src/game/entities/entity.dart';
+part 'package:couclient/src/game/entities/grounditem.dart';
+part 'package:couclient/src/game/entities/npc.dart';
+part 'package:couclient/src/game/entities/plant.dart';
+part 'package:couclient/src/game/entities/player.dart';
+part 'package:couclient/src/game/entities/quoin.dart';
+part 'package:couclient/src/game/entities/wormhole.dart';
+part 'package:couclient/src/game/game.dart';
 part 'package:couclient/src/game/street.dart';
 
-part 'package:couclient/src/game/entities/quoin.dart';
+// Globals
 
-part 'package:couclient/src/game/entities/grounditem.dart';
+final GpsIndicator gpsIndicator = new GpsIndicator(); // GPS status display
+final NumberFormat commaFormatter = new NumberFormat("#,###"); // Comma format
+final Random random = new Random(); // Random object
+final Storage localStorage = window.localStorage; // Local storage
+final Storage sessionStorage = window.sessionStorage; // Session storage
+final String rsToken = "ud6He9TXcpyOEByE944g"; // Token for redstone
 
-// UI PIECES //
-part 'package:couclient/src/display/ui_templates/interactions_menu.dart';
+Map<String, dynamic> constants; // Server-defined constants
+MapData mapData; // Map, street, and hub metadata
 
-part 'package:couclient/src/display/ui_templates/right_click_menu.dart';
-
-part 'package:couclient/src/display/ui_templates/howmany.dart';
-
-part 'package:couclient/src/display/ui_templates/menu_keys.dart';
-
-part 'package:couclient/src/display/ui_templates/item_chooser.dart';
-
-part 'package:couclient/src/display/minimap.dart';
-
-// Globals //
-Storage sessionStorage = window.sessionStorage;
-Storage localStorage = window.localStorage;
-Random random = new Random();
-MapWindow mapWindow;
-NumberFormat commaFormatter = new NumberFormat("#,###");
-SoundManager audio;
-WeatherManager weather;
-QuestManager questManager;
-InputManager inputManager;
-WindowManager windowManager;
-CommandManager commandManager;
-AuthManager auth;
-Game game;
-DateTime startTime;
-Minimap minimap;
-GpsIndicator gpsIndicator = new GpsIndicator();
-final String rsToken = "ud6He9TXcpyOEByE944g";
-MapData mapData;
-Map<String, dynamic> constants;
+AuthManager auth; // Auth server interop
+CommandManager commandManager; // Command input
+DateTime startTime; // Track times in log messages
+Game game; // Game object
+InputManager inputManager; // Input handler
+MapWindow mapWindow; // Single-instance map window
+Minimap minimap; // Minimap object
+QuestManager questManager; // Quest manager
+SoundManager audio; // Audio manager
+WeatherManager weather; // Weather manager
+WindowManager windowManager; // Window manager
 
 bool get hasTouchSupport => context.callMethod("hasTouchSupport");
 
