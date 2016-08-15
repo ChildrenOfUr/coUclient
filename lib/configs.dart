12da0ce5cbc96d4e1957db4235c0698347a1672e
Andy Castille <klikini@robiotic.net>
2016-08-14 19:24:21 -0500

    bump version 144

--- lib/configs.dart
+++ lib/configs.dart
@@ -5,7 +5,7 @@ import 'dart:async';
 
 class Configs {
 	static String baseAddress, utilServerAddress, websocketServerAddress, authAddress, authWebsocket;
-	static final int clientVersion = 143;
+	static final int clientVersion = 144;
 	static bool testing;
 
 	static Future init() async
