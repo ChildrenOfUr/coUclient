53fc02be88bd4e5823afdad44a9c88067f891043
Andy Castille <klikini@robiotic.net>
2016-10-23 11:20:35 -0500

    minimap exits show again, GPS arrow works again fixes https://github.com/ChildrenOfUr/cou-issues/issues/1971

--- lib/src/display/gps_display.dart
+++ lib/src/display/gps_display.dart
@@ -6,30 +6,34 @@ class GpsIndicator {
 	Element nextStreetE = querySelector("#gps-next");
 	Element destinationE = querySelector("#gps-destination");
 	Element arrowE = querySelector("#gps-direction");
-	bool loadingNew, arriveFade = false;
+	bool loadingNew,
+		arriveFade = false;
 
 	GpsIndicator() {
 		closeButtonE.onClick.listen((_) => this.cancel());
 	}
 
-	update() {
+	void update() {
 		if (!loadingNew) {
 			if (GPS.active) {
 				containerE.hidden = false;
 				nextStreetE.text = GPS.nextStreetName;
 				destinationE.text = GPS.destinationName;
+
 				if (GPS.nextStreetName == "You're off the path") {
 					GPS.getRoute(currentStreet.label, GPS.destinationName);
-				} else if(GPS.nextStreetName == "You have arrived") {
+				} else if (GPS.nextStreetName == "You have arrived") {
 					arrowE.querySelector(".fa").classes
 						..removeAll(["fa-arrow-up", "fa-check"])
 						..add("fa-check");
 					arrowE.style.transform = "rotate(0rad)";
-					if(arriveFade || !currentStreet.loaded) {
+
+					if (arriveFade || !currentStreet.loaded) {
 						return;
 					}
+
 					arriveFade = true;
-					new Timer(new Duration(seconds:3),(){
+					new Timer(new Duration(seconds: 3), () {
 						arriveFade = false;
 						cancel();
 					});
@@ -44,7 +48,7 @@ class GpsIndicator {
 		}
 	}
 
-	cancel() {
+	void cancel() {
 		if (GPS.active) {
 			GPS.currentRoute.clear();
 			GPS.active = false;
@@ -66,28 +70,29 @@ class GpsIndicator {
 		num playerX = CurrentPlayer.left;
 		num playerY = CurrentPlayer.top + CurrentPlayer.height / 2;
 
-		for(Map exit in minimap.currentStreetExits) {
-			if(exit["streets"].contains(GPS.nextStreetName)) {
-				if(exit["streets"].length > 1) {
+		for (Map exit in minimap.currentStreetExits) {
+			if (exit["streets"].contains(GPS.nextStreetName)) {
+				if (exit["streets"].length > 1) {
 					// signs on both sides
 					exitX = exit["x"] + 100;
 				} else {
 					exitX = exit["x"];
 				}
+
 				exitY = exit["y"] + 115; // center of the height
 				break;
 				// no point in wasting time, we found the signpost
 			}
 		};
 
-		if(exitX == null || exitY == null) {
-			return 0;
+		if (exitX == null || exitY == null) {
 			// error
+			return 0;
 		}
 
 		num dy = exitY - playerY;
 		num dx = exitX - playerX;
-		num theta = atan2(dy, dx) - 3*PI/2;
+		num theta = atan2(dy, dx) - 3 * PI / 2;
 		return theta;
 	}
 }
