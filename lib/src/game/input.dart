70ba6133ee958cfeedc8486f75b7f7920f5349e4
Andy Castille <klikini@robiotic.net>
2017-03-14 19:09:45 -0500

    input.dart...yikes

--- lib/src/game/input.dart
+++ lib/src/game/input.dart
@@ -1,18 +1,19 @@
 part of couclient;
 
+/// Needed for passing objects by reference
 class Bool {
 	bool value = false;
 
 	Bool([this.value]);
 
-	bool operator ==(other)
-	{
-		if (other is bool)
+	bool operator ==(other) {
+		if (other is bool) {
 			return this.value == other;
-		if (other is Bool)
+		} else if (other is Bool) {
 			return this.value == other.value;
-		else
+		} else {
 			return this == other;
+		}
 	}
 
 	@override
@@ -21,8 +22,13 @@ class Bool {
 
 class InputManager {
 	bool windowFocused = true;
-	Bool rightKey = new Bool(), leftKey = new Bool(), upKey = new Bool(),
-	downKey = new Bool(), jumpKey = new Bool(), actionKey = new Bool();
+	Bool rightKey = new Bool(false);
+	Bool leftKey = new Bool(false);
+	Bool upKey = new Bool(false);
+	Bool downKey = new Bool(false);
+	Bool jumpKey = new Bool(false);
+	Bool actionKey = new Bool(false);
+
 	Map<String, int> keys = {
 		"LeftBindingPrimary": 65,
 		"LeftBindingAlt": 37,
@@ -37,7 +43,7 @@ class InputManager {
 		"ActionBindingPrimary": 13,
 		"ActionBindingAlt": 69,
 		"MapBindingPrimary": 77,
-		"MapBindingAlt":77,
+		"MapBindingAlt": 77,
 		"CalendarBindingPrimary": 67,
 		"CalendarBindingAlt": 67,
 		"SettingsBindingPrimary": 80,
@@ -51,38 +57,42 @@ class InputManager {
 		"AchievementsBindingPrimary": 89,
 		"AchievementsBindingAlt": 89
 	};
+
 	int _ignoreCount = 0;
+
 	bool get ignoreKeys => _ignoreCount != 0;
+
 	void set ignoreKeys(bool value) {
 		if (value) {
 			_ignoreCount++;
-		} else {
-			_ignoreCount--;
-			if (_ignoreCount < 0) {
-				_ignoreCount = 0;
-			}
+		} else if (--_ignoreCount < 0) {
+			_ignoreCount = 0;
 		}
 	}
-	bool ignoreChatFocus = false,
-	touched = false,
-	clickUsed = false;
-	StreamSubscription keyPressSub, keyDownSub, menuKeyListener;
+
+	bool ignoreChatFocus = false;
+	bool touched = false;
+	bool clickUsed = false;
+	StreamSubscription keyPressSub;
+	StreamSubscription keyDownSub;
+	StreamSubscription menuKeyListener;
 	DateTime lastSelect = new DateTime.now();
 	Map<String, Map<String, dynamic>> controlCounts = {};
 
 	InputManager() {
-		controlCounts = {'leftKey':{'signals':{}, 'keyBool':leftKey},
-			'rightKey':{'signals':{}, 'keyBool':rightKey},
-			'upKey':{'signals':{}, 'keyBool':upKey},
-			'downKey':{'signals':{}, 'keyBool':downKey},
-			'jumpKey':{'signals':{}, 'keyBool':jumpKey},
-			'actionKey':{'signals':{}, 'keyBool':actionKey}
+		controlCounts = {
+			'leftKey': {'signals': {}, 'keyBool': leftKey},
+			'rightKey': {'signals': {}, 'keyBool': rightKey},
+			'upKey': {'signals': {}, 'keyBool': upKey},
+			'downKey': {'signals': {}, 'keyBool': downKey},
+			'jumpKey': {'signals': {}, 'keyBool': jumpKey},
+			'actionKey': {'signals': {}, 'keyBool': actionKey},
 		};
 
 		setupKeyBindings();
 
-		new Service(['disableInputKeys'],(bool value) => ignoreKeys = value);
-		new Service(['disableChatFocus'],(bool value) => ignoreChatFocus = value);
+		new Service(['disableInputKeys'], (bool value) => ignoreKeys = value);
+		new Service(['disableChatFocus'], (bool value) => ignoreChatFocus = value);
 
 		document.onClick.listen((MouseEvent event) => clickOrTouch(event, null));
 		document.onTouchStart.listen((TouchEvent event) => clickOrTouch(null, event));
@@ -94,10 +104,10 @@ class InputManager {
 		window.onBlur.listen((_) => windowFocused = false);
 	}
 
-	activateControl(String control, bool active, String sourceName) {
+	void activateControl(String control, bool active, String sourceName) {
 		Map<String, Map> signalsList = controlCounts[control]['signals'];
 		if (active && !signalsList.containsKey(sourceName + '-' + control)) {
-			signalsList[sourceName + '-' + control] = {'sourceName':sourceName, 'active':active};
+			signalsList[sourceName + '-' + control] = {'sourceName': sourceName, 'active': active};
 		} else if (!active) {
 			signalsList.remove(sourceName + '-' + control);
 		}
@@ -120,17 +130,20 @@ class InputManager {
 		});
 	}
 
-	updateGamepad() {
+	void updateGamepad() {
 		//get any gamepads
 		List<Gamepad> gamepads = window.navigator.getGamepads();
 
-		for(Gamepad gamepad in gamepads) {
+		for (Gamepad gamepad in gamepads) {
 			//the list of gamepads the browser returns can include nulls
-			if (gamepad == null)
+			if (gamepad == null) {
 				continue;
+			}
 
 			//don't do anything in certain situations
-			if (ignoreKeys || ActionBubble.occuring) return;
+			if (ignoreKeys || ActionBubble.occuring) {
+				return;
+			}
 
 			//interact with the menu
 			Element clickMenu = querySelector("#RightClickMenu");
@@ -145,10 +158,9 @@ class InputManager {
 				if (controlCounts['downKey']['keyBool'] == true && selectAgain) {
 					selectDown(list.querySelectorAll('.RCItem'), "RCItemSelected");
 				}
-				if (
-				controlCounts['leftKey']['keyBool'] == true ||
-				controlCounts['rightKey']['keyBool'] == true ||
-				controlCounts['jumpKey']['keyBool'] == true) {
+				if (controlCounts['leftKey']['keyBool'] == true ||
+					controlCounts['rightKey']['keyBool'] == true ||
+					controlCounts['jumpKey']['keyBool'] == true) {
 					stopMenu(clickMenu);
 				}
 			}
@@ -180,39 +192,41 @@ class InputManager {
 			bool button0 = context['navigator'].callMethod('getGamepads')[gamepad.index]['buttons'][0]['pressed'];
 			bool button1 = context['navigator'].callMethod('getGamepads')[gamepad.index]['buttons'][1]['pressed'];
 
-			if (button0)
+			if (button0) {
 				activateControl('jumpKey', true, 'gamepad');
-			else
+			} else {
 				activateControl('jumpKey', false, 'gamepad');
+			}
 
 			if (button1) {
 				if (controlCounts['actionKey']['keyBool'] == false) {
 					activateControl('actionKey', true, 'gamepad');
 					doObjectInteraction();
 				}
-			}
-			else
+			} else {
 				activateControl('actionKey', false, 'gamepad');
+			}
 		}
 	}
 
-	clickOrTouch(MouseEvent mouseEvent, TouchEvent touchEvent) {
+	void clickOrTouch(MouseEvent mouseEvent, TouchEvent touchEvent) {
 		// TODO: for now mobile touch targets are not included
 		//don't handle too many touch events too fast
-		if (touched) return;
+		if (touched) {
+			return;
+		}
+
 		touched = true;
 		new Timer.periodic(new Duration(milliseconds: 200), (Timer timer) {
 			timer.cancel();
 			touched = false;
 		});
-		Element target;
 
-		if (mouseEvent != null) target = mouseEvent.target;
-		else target = touchEvent.target;
+		Element target = (mouseEvent != null) ? mouseEvent.target : touchEvent.target;
 
-		activatePresetBindings(String preset) {
+		void activatePresetBindings(String preset) {
 			// check which preset movement keys to set & apply them
-			switch(preset) {
+			switch (preset) {
 				case "qwerty":
 				// set WASD
 					localStorage["LeftBindingPrimary"] = "65.97";
@@ -294,10 +308,10 @@ class InputManager {
 		}
 	}
 
-	setupKeyBindings() {
+	void setupKeyBindings() {
 		//this prevents 2 keys from being set at once
-		if (keyPressSub != null) keyPressSub.cancel();
-		if (keyDownSub != null) keyDownSub.cancel();
+		keyPressSub?.cancel();
+		keyDownSub?.cancel();
 
 		//set up key bindings
 		keys.forEach((String action, int keyCode) {
@@ -305,32 +319,31 @@ class InputManager {
 			if (localStorage[action] != null) {
 				storedValue = localStorage[action].split(".");
 				keys[action] = int.parse(storedValue[0]);
+			} else {
+				localStorage[action] = keys[action].toString();
 			}
-			else localStorage[action] = keys[action].toString();
 
 			String key = fromKeyCode(keys[action]);
 			if (key == "") {
-				if (storedValue != null && storedValue.length > 1) querySelector("#$action").text = new String.fromCharCode(int.parse(storedValue[1])).toUpperCase();
-				else querySelector("#$action").text = new String.fromCharCode((keys[action]));
+				if (storedValue != null && storedValue.length > 1) {
+					querySelector("#$action").text = new String.fromCharCode(int.parse(storedValue[1])).toUpperCase();
+				} else {
+					querySelector("#$action").text = new String.fromCharCode((keys[action]));
+				}
+			} else {
+				querySelector("#$action").text = key;
 			}
-			else querySelector("#$action").text = key;
 		});
-
 
 		PaperToggleButton graphicsBlur = querySelector("#GraphicsBlur") as PaperToggleButton;
 		graphicsBlur.onChange.listen((_) {
 			localStorage["GraphicsBlur"] = graphicsBlur.checked.toString();
 		});
 
-
 		//Handle player input
 		//KeyUp and KeyDown are neccesary for preventing weird movement glitches
 		document.onKeyDown.listen((KeyboardEvent k) {
-			if (menuKeyListener != null) {
-				return;
-			}
-
-			if (ignoreChatFocus) {
+			if (menuKeyListener != null || ignoreChatFocus) {
 				return;
 			}
 
@@ -339,40 +352,14 @@ class InputManager {
 				advanceChatFocus(k);
 			}
 
-			if (ignoreKeys || ActionBubble.occuring) return;
-
-			if ((k.keyCode == keys["UpBindingPrimary"] || k.keyCode == keys["UpBindingAlt"])) //up arrow or w
-				activateControl('upKey', true, 'keyboard');
-			if ((k.keyCode == keys["DownBindingPrimary"] || k.keyCode == keys["DownBindingAlt"])) //down arrow or s
-				activateControl('downKey', true, 'keyboard');
-			if ((k.keyCode == keys["LeftBindingPrimary"] || k.keyCode == keys["LeftBindingAlt"])) //left arrow or a
-				activateControl('leftKey', true, 'keyboard');
-			if ((k.keyCode == keys["RightBindingPrimary"] || k.keyCode == keys["RightBindingAlt"])) //right arrow or d
-				activateControl('rightKey', true, 'keyboard');
-			if ((k.keyCode == keys["JumpBindingPrimary"] || k.keyCode == keys["JumpBindingAlt"])) //spacebar
-				activateControl('jumpKey', true, 'keyboard');
-			if ((k.keyCode == keys["ActionBindingPrimary"] || k.keyCode == keys["ActionBindingAlt"])) //enter
-				doObjectInteraction();
-		});
-
-		document.onKeyUp.listen((KeyboardEvent k) {
-			if (ignoreKeys) {
+			if (ActionBubble.occuring) {
 				return;
 			}
 
-			if ((k.keyCode == keys["UpBindingPrimary"] || k.keyCode == keys["UpBindingAlt"])) //up arrow or w
-				activateControl('upKey', false, 'keyboard');
-			if ((k.keyCode == keys["DownBindingPrimary"] || k.keyCode == keys["DownBindingAlt"])) //down arrow or s
-				activateControl('downKey', false, 'keyboard');
-			if ((k.keyCode == keys["LeftBindingPrimary"] || k.keyCode == keys["LeftBindingAlt"])) //left arrow or a
-				activateControl('leftKey', false, 'keyboard');
-			if ((k.keyCode == keys["RightBindingPrimary"] || k.keyCode == keys["RightBindingAlt"])) //right arrow or d
-				activateControl('rightKey', false, 'keyboard');
-			if ((k.keyCode == keys["JumpBindingPrimary"] || k.keyCode == keys["JumpBindingAlt"])) //spacebar
-				activateControl('jumpKey', false, 'keyboard');
-			if ((k.keyCode == keys["ActionBindingPrimary"] || k.keyCode == keys["ActionBindingAlt"])) //enter
-				activateControl('actionKey', false, 'keyboard');
+			updateKeys(true, k.keyCode);
 		});
+
+		document.onKeyUp.listen((KeyboardEvent k) => updateKeys(false, k.keyCode));
 
 		//listen for right-clicks on entities that we're close to
 		document.body.onContextMenu.listen((MouseEvent e) {
@@ -381,45 +368,49 @@ class InputManager {
 			}
 			//just like pressing a key for 10ms
 			doObjectInteraction();
-			new Timer(new Duration(milliseconds:10),(){
+			new Timer(new Duration(milliseconds: 10), () {
 				activateControl('actionKey', false, 'mouse');
 			});
 		});
 
 		//only for mobile version
-		Joystick joystick = new Joystick(querySelector('#Joystick'), querySelector('#Knob'), deadzoneInPercent:.2);
+		Joystick joystick = new Joystick(querySelector('#Joystick'), querySelector('#Knob'), deadzoneInPercent: .2);
+
 		joystick.onMove.listen((_) {
 			//don't move during harvesting, etc.
 			if (!ActionBubble.occuring) {
-				if (joystick.UP) activateControl('upKey', true, 'joystick');
-				else activateControl('upKey', false, 'joystick');
-				if (joystick.DOWN) activateControl('downKey', true, 'joystick');
-				else activateControl('downKey', false, 'joystick');
-				if (joystick.LEFT) activateControl('leftKey', true, 'joystick');
-				else activateControl('leftKey', false, 'joystick');
-				if (joystick.RIGHT) activateControl('rightKey', true, 'joystick');
-				else activateControl('rightKey', false, 'joystick');
+				activateControl('upKey', joystick.UP, 'joystick');
+				activateControl('downKey', joystick.DOWN, 'joystick');
+				activateControl('leftKey', joystick.LEFT, 'joystick');
+				activateControl('rightKey', joystick.RIGHT, 'joystick');
 			}
 
 			Element clickMenu = querySelector("#RightClickMenu");
 			if (clickMenu != null) {
 				Element list = querySelector('#RCActionList');
 				//only select a new option once every 300ms
-				bool selectAgain = lastSelect.add(new Duration(milliseconds:300)).isBefore(new DateTime.now());
-				if (joystick.UP && selectAgain)
-					selectUp(list.querySelectorAll('.RCItem'), "RCItemSelected");
-				if (joystick.DOWN && selectAgain)
-					selectDown(list.querySelectorAll('.RCItem'), "RCItemSelected");
-				if (joystick.LEFT || joystick.RIGHT)
+				if (lastSelect.add(new Duration(milliseconds: 300)).isBefore(new DateTime.now())) {
+					if (joystick.UP) {
+						selectUp(list.querySelectorAll('.RCItem'), "RCItemSelected");
+					}
+
+					if (joystick.DOWN) {
+						selectDown(list.querySelectorAll('.RCItem'), "RCItemSelected");
+					}
+				}
+
+				if (joystick.LEFT || joystick.RIGHT) {
 					stopMenu(clickMenu);
+				}
 			}
 		});
+
 		joystick.onRelease.listen((_) {
-			activateControl('upKey', false, 'joystick');
-			activateControl('downKey', false, 'joystick');
-			activateControl('rightKey', false, 'joystick');
-			activateControl('leftKey', false, 'joystick');
+			for (String key in ['up', 'down', 'right', 'left']) {
+				activateControl(key + 'Key', false, 'joystick');
+			}
 		});
+
 		document.onTouchStart.listen((TouchEvent event) {
 			Element target = event.target;
 
@@ -433,6 +424,7 @@ class InputManager {
 				doObjectInteraction();
 			}
 		});
+
 		document.onTouchEnd.listen((TouchEvent event) {
 			Element target = event.target;
 
@@ -448,14 +440,34 @@ class InputManager {
 		//end mobile specific stuff
 	}
 
+	void updateKeys(bool newState, int keyCode) {
+		bool _checkControl(String control) =>
+			keyCode == keys[control + 'BindingPrimary'] || keyCode == keys[control + 'BindingAlt'];
+
+		if (ignoreKeys) {
+			return;
+		}
+
+		for (String key in ['Up', 'Down', 'Left', 'Right', 'Jump', 'Action']) {
+			if (!_checkControl(key)) {
+				continue;
+			}
+
+			if (newState && key == 'Action') {
+				doObjectInteraction();
+			} else {
+				activateControl(key.toLowerCase() + 'Key', newState, 'keyboard');
+			}
+		}
+	}
+
 	void doObjectInteraction([MouseEvent e, List<String> ids]) {
 		if (querySelector("#RightClickMenu") != null) {
 			doAction(querySelector('#RCActionList'), querySelector("#RightClickMenu"), "RCItemSelected");
 			return;
 		}
 
-		if (
-			CurrentPlayer.intersectingObjects.length > 0
+		if (CurrentPlayer.intersectingObjects.length > 0
 			&& querySelector('#RightClickMenu') == null
 			&& !ActionBubble.occuring
 		) {
@@ -469,18 +481,14 @@ class InputManager {
 	}
 
 	void createMultiEntityWindow() {
-		Element oldWindow = querySelector("#InteractionWindow");
-		if (oldWindow != null) {
-			oldWindow.remove();
-		}
-
+		querySelector("#InteractionWindow")?.remove();
 		document.body.append(InteractionWindow.create());
 	}
 
 	// Right-click menu functions
-	hideClickMenu(Element window) => window?.remove();
+	void hideClickMenu(Element window) => window?.remove();
 
-	showClickMenu({
+	void showClickMenu({
 		MouseEvent click,
 		String title: 'Interact',
 		String description: 'Desc',
@@ -521,7 +529,7 @@ class InputManager {
 
 	void selectUp(List<Element> options, String className) {
 		int removed = 0;
-		for(int i = 0; i < options.length; i++) {
+		for (int i = 0; i < options.length; i++) {
 			options[i].classes.remove('action_hover');
 			if (options[i].classes.remove(className)) {
 				removed = i;
@@ -540,7 +548,7 @@ class InputManager {
 
 	void selectDown(List<Element> options, String className) {
 		int removed = options.length - 1;
-		for(int i = 0; i < options.length; i++) {
+		for (int i = 0; i < options.length; i++) {
 			options[i].classes.remove('action_hover');
 			if (options[i].classes.remove(className)) {
 				removed = i;
@@ -559,87 +567,87 @@ class InputManager {
 
 	void stopMenu(Element window) {
 		transmit('menuStopping', window);
-
-		if (menuKeyListener != null) {
-			menuKeyListener.cancel();
-			menuKeyListener = null;
-		}
+		menuKeyListener?.cancel();
+		menuKeyListener = null;
 		MenuKeys.clearListeners();
 		hideClickMenu(window);
 	}
 
 	void doAction(Element list, Element window, String className) {
-		for(Element element in list.querySelectorAll('.RCItem')) {
+		for (Element element in list.querySelectorAll('.RCItem')) {
 			if (element.classes.contains(className)) {
 				//click the hard way so that we have coordinates to go with it
 				Rectangle rect = element.getBoundingClientRect();
-				element.dispatchEvent(new MouseEvent('click', clientX: (rect.left+rect.width~/2)~/1, clientY: rect.top~/1));
+				element.dispatchEvent(new MouseEvent('click', clientX: (rect.left + rect.width ~/ 2) ~/ 1, clientY: rect.top ~/ 1));
 				break;
 			}
 		}
 		stopMenu(window);
 	}
 
-	String fromKeyCode(int keyCode) => const {
-		8: "backspace",
-		9: "tab",
-		13: "enter",
-		16: "shift",
-		17: "ctrl",
-		18: "left alt",
-		19: "pause/break",
-		20: "caps lock",
-		27: "escape",
-		32: "space",
-		33: "page up",
-		34: "page down",
-		35: "end",
-		36: "home",
-		37: "left arrow",
-		38: "up arrow",
-		39: "right arrow",
-		40: "down arrow",
-		45: "insert",
-		46: "delete",
-		91: "left window",
-		92: "right window",
-		93: "select key",
-		96: "numpad 0",
-		97: "numpad 1",
-		98: "numpad 2",
-		99: "numpad 3",
-		100: "numpad 4",
-		101: "numpad 5",
-		102: "numpad 6",
-		103: "numpad 7",
-		104: "numpad 8",
-		105: "numpad 9",
-		106: "multiply",
-		107: "add",
-		109: "subtract",
-		110: "decimal point",
-		111: "divide",
-		112: "F1",
-		113: "F2",
-		114: "F3",
-		115: "F4",
-		116: "F5",
-		117: "F6",
-		118: "F7",
-		119: "F8",
-		120: "F9",
-		121: "F10",
-		122: "F11",
-		123: "F12",
-		144: "num lock",
-		145: "scroll lock",
-		225: "right alt",
-	}[keyCode] ?? "";
+	String fromKeyCode(int keyCode) =>
+		const {
+			8: "backspace",
+			9: "tab",
+			13: "enter",
+			16: "shift",
+			17: "ctrl",
+			18: "left alt",
+			19: "pause/break",
+			20: "caps lock",
+			27: "escape",
+			32: "space",
+			33: "page up",
+			34: "page down",
+			35: "end",
+			36: "home",
+			37: "left arrow",
+			38: "up arrow",
+			39: "right arrow",
+			40: "down arrow",
+			45: "insert",
+			46: "delete",
+			91: "left window",
+			92: "right window",
+			93: "select key",
+			96: "numpad 0",
+			97: "numpad 1",
+			98: "numpad 2",
+			99: "numpad 3",
+			100: "numpad 4",
+			101: "numpad 5",
+			102: "numpad 6",
+			103: "numpad 7",
+			104: "numpad 8",
+			105: "numpad 9",
+			106: "multiply",
+			107: "add",
+			109: "subtract",
+			110: "decimal point",
+			111: "divide",
+			112: "F1",
+			113: "F2",
+			114: "F3",
+			115: "F4",
+			116: "F5",
+			117: "F6",
+			118: "F7",
+			119: "F8",
+			120: "F9",
+			121: "F10",
+			122: "F11",
+			123: "F12",
+			144: "num lock",
+			145: "scroll lock",
+			225: "right alt",
+		}[keyCode] ?? "";
 
-	bool konamiDone = false, freeTeleportUsed = false;
-	initKonami() {
-		List<int> konamiKeys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-		String konami = konamiKeys.toString().replaceAll("[", "").replaceAll("]", "");
+	bool konamiDone = false;
+	bool freeTeleportUsed = false;
+
+	void initKonami() {
+		final List<int> konamiKeys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+		final String konami = konamiKeys.join(", ");
 		List<int> keys = [];
 
 		document.onKeyDown.listen((KeyboardEvent e) {
